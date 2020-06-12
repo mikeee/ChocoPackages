7d003484f4a79cd2ddbca39115fcc0210e086336
mikeee <hey@mike.ee>
2020-06-12 02:01:37 +0100

    update hcloud package

--- automatic/hcloud/update.ps1
+++ automatic/hcloud/update.ps1
@@ -1,10 +1,20 @@
-. $PSScriptRoot\..\hcloud.portable\update.ps1
+#import-module au
 
 function global:au_SearchReplace {
-   @{
+    @{
         "$($Latest.PackageName).nuspec" = @{
             "(\<dependency .+?`"$($Latest.PackageName).portable`" version=)`"([^`"]+)`"" = "`$1`"$($Latest.Version)`""
         }
+    }
+}
+
+function global:au_BeforeUpdate { }
+
+function global:au_GetLatest {
+    (clist hcloud.portable -e --by-id-only | select -Skip 1 | select -SkipLast 1) -match '^.+?\s+(?<version>.+?)\s+'
+    
+    return @{
+        Version = $matches.version
     }
 }
 
