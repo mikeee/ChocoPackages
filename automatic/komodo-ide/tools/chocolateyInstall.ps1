b811092d35085de0ee5ad41e4824c9caee125f78
mikeee <hey@mike.ee>
2020-06-12 21:17:46 +0100

    [AU komodo-ide]

---
+++ automatic/komodo-ide/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'MSI'
+  url           = ''
+  checksum      = ''
+  checksumType  = 'SHA256'
+
+  softwareName  = 'komodo-ide'
+
+  silentArgs    = "/quiet /norestart /l*v `"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
+Write-Verbose "Komodo IDE install log file is available at '$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).MsiInstall.log'"
