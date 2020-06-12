00fe8fd589bdc887934de559fe0388e6b10c3be6
mikeee <chocolatey@mike.ee>
2020-06-12 21:29:23 +0000

    AU: 1 updated - komodo-ide [skip ci] https://gist.github.com/17b5a042cba1bd9da2bf21615aab4f0f/c33f2d41e517508a7d91046b3fc0b9bfd3cdff44

--- automatic/komodo-ide/tools/chocolateyInstall.ps1
+++ automatic/komodo-ide/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'MSI'
-  url           = ''
-  checksum      = ''
+  url           = 'https://downloads.activestate.com/Komodo/releases/12.0.1/Komodo-IDE-12.0.1-91869.msi'
+  checksum      = '9a6d8628add6cb2af32fa3dc5cdc8144495ee031ecc7364d08b85ad305b2077c'
   checksumType  = 'SHA256'
 
   softwareName  = 'komodo-ide'
