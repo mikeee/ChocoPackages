660051fc5035227d664f74d1db406db289c0f389
mikeee <hey@mike.ee>
2020-06-22 23:08:41 +0100

    add package op

---
+++ automatic/op/README.md
@@ -0,0 +1,7 @@
+# ![op Logo](https://cdn.jsdelivr.net/gh/mikeee/ChocoPackages/icons/op.png "op Logo") [op](https://chocolatey.org/packages/op)
+
+1Password is available with just two characters: `op`.
+
+The 1Password command-line tool makes your 1Password account accessible entirely from the command line. A simple op signin will securely authenticate you with the 1Password service and give you access to a wide range of capabilities.
+
+**NOTE**: This package is automatically updated. If it is out of date and there is no newer package submitted, please use the `Contact Maintainers` functionality to alert me.
---
+++ automatic/op/update.ps1
@@ -0,0 +1,47 @@
+#import-module au
+
+. $PSScriptRoot\..\..\scripts\all.ps1
+
+$releases    = 'https://app-updates.agilebits.com/product_history/CLI'
+
+function global:au_SearchReplace {
+    @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            '(^\s*url\s*=\s*)(''.*'')'              = "`$1'$($Latest.URL32)'"
+            "(?i)(^\s*checksum\s*=\s*)('.*')"       = "`$1'$($Latest.Checksum32)'"
+            "(?i)(^\s*checksumType\s*=\s*)('.*')"   = "`$1'$($Latest.ChecksumType32)'"
+            '(^\s*url64\s*=\s*)(''.*'')'            = "`$1'$($Latest.URL64)'"
+            "(?i)(^\s*checksum64\s*=\s*)('.*')"       = "`$1'$($Latest.Checksum64)'"
+            "(?i)(^\s*checksumType64\s*=\s*)('.*')"   = "`$1'$($Latest.ChecksumType64)'"
+        }
+    }
+}
+
+function global:au_BeforeUpdate {
+    $Latest.Checksum32 = Get-RemoteChecksum $Latest.Url32
+    $Latest.ChecksumType32 = 'SHA256'
+    $Latest.Checksum64 = Get-RemoteChecksum $Latest.Url64
+    $Latest.ChecksumType64 = 'SHA256'
+}
+
+function global:au_AfterUpdate {
+    Set-DescriptionFromReadme -SkipFirst 2
+}
+
+function global:au_GetLatest {
+    $page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+    $regexUrl32 = '\/op_windows_386_v(?<version>[\d\.]+).zip'
+    $regexUrl64 = '\/op_windows_amd64_v(?<version>[\d\.]+).zip'
+
+    $url32 = $page.links | Where-Object href -match $regexUrl32 | Select-Object -First 1 -expand href
+
+    $url64 = $page.links | Where-Object href -match $regexUrl64 | Select-Object -First 1 -expand href
+
+    return @{
+        URL32        = $url32
+        URL64        = $url64
+        Version      = $matches.version
+    }
+}
+
+update -ChecksumFor none
