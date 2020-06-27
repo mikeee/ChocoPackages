0b55050a01ad006ace49597d2e9000ef427305f9
mikeee <chocolatey@mike.ee>
2020-06-27 22:03:53 +0000

    AU: 1 updated - komodo-edit [skip ci] https://gist.github.com/17b5a042cba1bd9da2bf21615aab4f0f/9c70205db9c09b93013cd82ef974cee84250c77d

--- automatic/komodo-edit/tools/chocolateyInstall.ps1
+++ automatic/komodo-edit/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
-$ErrorActionPreference = 'Stop'
+﻿$ErrorActionPreference = 'Stop'
 
 $packageArgs = @{
   packageName     = $env:ChocolateyPackageName
   fileType        = 'MSI'
-  url64           = ''
-  checksum64      = ''
+  url64           = 'https://downloads.activestate.com/Komodo/releases/12.0.1/Komodo-Edit-12.0.1-18441.msi'
+  checksum64      = '8d677839b7c3651f43df2c1e12ec5f2a6422e4e86b9617dab74563d470988d54'
   checksumType64  = 'SHA256'
 
   softwareName    = 'komodo-edit'
