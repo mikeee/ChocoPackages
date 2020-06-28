71c5a0d5e6ae7f0ebdb32d2c585409b69ff1cb4f
mikeee <chocolatey@mike.ee>
2020-06-28 01:42:05 +0000

    AU: 1 updated - ossec-agent [skip ci] https://gist.github.com/17b5a042cba1bd9da2bf21615aab4f0f/c97098d6ca29b995c561f4953c5addfd3df6d177

--- automatic/ossec-agent/tools/chocolateyInstall.ps1
+++ automatic/ossec-agent/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
-$ErrorActionPreference = 'Stop';
+﻿$ErrorActionPreference = 'Stop';
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   installerType  = 'exe'
-  url            = ''
-  checksum       = ''
+  url            = 'https://updates.atomicorp.com/channels/atomic/windows/ossec-agent-win32-3.6.0-12032.exe'
+  checksum       = '1dcaa817ce9b9fd992787b070fabc8e78c8fe13d07abe031e326b6b7dbb5e519'
   checksumType   = 'SHA256'
   silentArgs     = '/S'
   validExitCodes = @(0)
