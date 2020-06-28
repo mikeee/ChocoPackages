8686f75c1167cfbf64d2f42bda0631cdc3fe40d9
mikeee <chocolatey@mike.ee>
2020-06-28 13:33:54 +0000

    AU: 1 updated - crystaldiskmark [skip ci] https://gist.github.com/17b5a042cba1bd9da2bf21615aab4f0f/d484bd651021c064cc2d8d5caf432cc2c166fa49

--- automatic/crystaldiskmark/tools/chocolateyInstall.ps1
+++ automatic/crystaldiskmark/tools/chocolateyInstall.ps1
@@ -4,8 +4,8 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
     unzipLocation  = $toolsDir
-    url            = ''
-    checksum       = ''
+    url            = 'https://osdn.net/frs/redir.php?m=dotsrc&f=crystaldiskmark/71859/CrystalDiskMark7_0_0h.exe'
+    checksum       = '6b908d70ee1ac865a96f8fbee3c7b9f960d357cd5c02a43dac082f498c99514b'
     checksumType   = 'SHA256'
     fileType       = 'EXE'
     silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
