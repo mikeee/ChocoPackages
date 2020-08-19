1bd9ac6b5e7b1d869713a6f79ffa93e365050f90
mikeee <hey@mike.ee>
2020-08-19 06:25:28 +0100

    googlearthpro update to july re-sign from google

--- automatic/googleearthpro/tools/chocolateyInstall.ps1
+++ automatic/googleearthpro/tools/chocolateyInstall.ps1
@@ -3,10 +3,10 @@
 $packageArgs = @{
     packageName   = $env:ChocolateyPackageName
     url           = 'https://dl.google.com/dl/earth/client/advanced/current/googleearthprowin-7.3.3.exe'
-    checksum      = '642623f3f7f74dcf073fbaadac14950f0ca6c2bdfcf86c32e5d3e9cb2beba576'
+    checksum      = '30f4db70ade8c80bc40c1cc44f22ab725292f5f84d15dda8f7958891adcfc2c9'
     checksumType  = 'SHA256'
     url64         = 'https://dl.google.com/dl/earth/client/advanced/current/googleearthprowin-7.3.3-x64.exe'
-    checksum64    = 'db973894d401a715fd745d604fabee64649a8d6c4c0390aa883b61f65bb47fdb'
+    checksum64    = 'fe2711c7cb75455b52d7e4a6700be296fd567af607d5d43bfa8ba7451bb7c2b7'
     checksumType64= 'SHA256'
     fileType      = 'EXE'
     silentArgs    = 'OMAHA=1'
