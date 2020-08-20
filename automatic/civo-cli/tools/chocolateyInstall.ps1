7af52d07c753350714946b9e6209e254add96c1e
mikeee <chocolatey@mike.ee>
2020-08-20 16:01:05 +0000

    AU: 1 updated - civo-cli [skip ci] https://gist.github.com/17b5a042cba1bd9da2bf21615aab4f0f/b1732370846ff1fae2c45eb4739fc91d599df722

--- automatic/civo-cli/tools/chocolateyInstall.ps1
+++ automatic/civo-cli/tools/chocolateyInstall.ps1
@@ -5,12 +5,12 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
     unzipLocation  = $toolsDir
-    url            = 'https://github.com//civo/cli/releases/download/v0.6.26/civo-0.6.26-windows-386.zip'
-    checksum       = 'b5ede25ff23da08fbad55059585e1aa9651a88f3f7b53687be9830b0eeedf518'
+    url            = 'https://github.com//civo/cli/releases/download/v0.6.27/civo-0.6.27-windows-386.zip'
+    checksum       = 'f279dfb749d313d9a976f22f0a7a151c0cf4b11a2fc4483d10188045695b9193'
     checksumType   = 'SHA256'
 
-    url64          = 'https://github.com//civo/cli/releases/download/v0.6.26/civo-0.6.26-windows-amd64.zip'
-    checksum64     = '897fc0bb560c39c62f77d263dc7bf3fe776915a87dae86758eb555eb183e3817'
+    url64          = 'https://github.com//civo/cli/releases/download/v0.6.27/civo-0.6.27-windows-amd64.zip'
+    checksum64     = '9f6ea743f34efc1120918daa99d2e0f25fb6ab0aa053a09a96af1129aa90344b'
     checksumType64 = 'SHA256'
 }
 
