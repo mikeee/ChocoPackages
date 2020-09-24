601c92f6a53b350696fdeb7eb5369298c44523a3
TheCakeIsNaOH <TheCakeIsNaOH@gmail.com>
2020-09-24 16:56:48 -0500

    CrystalDiskMark move internal

--- automatic/crystaldiskmark/tools/chocolateyInstall.ps1
+++ automatic/crystaldiskmark/tools/chocolateyInstall.ps1
@@ -3,13 +3,12 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
-    unzipLocation  = $toolsDir
-    url            = 'https://osdn.net/frs/redir.php?m=dotsrc&f=crystaldiskmark/71859/CrystalDiskMark7_0_0h.exe'
-    checksum       = '6b908d70ee1ac865a96f8fbee3c7b9f960d357cd5c02a43dac082f498c99514b'
-    checksumType   = 'SHA256'
+    file           = Get-Item "$toolsDir\*.exe"
     fileType       = 'EXE'
     silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
     validExitCodes= @(0, 3010, 1641)
 }
 
-Install-ChocolateyPackage @packageArgs
+Install-ChocolateyInstallPackage @packageArgs
+
+Get-ChildItem $toolsDir\*.exe | ForEach-Object { Remove-Item $_ -ea 0; if (Test-Path $_) { Set-Content "$_.ignore" } }
