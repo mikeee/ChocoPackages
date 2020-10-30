8c44676330d697c16be34fee4dab1c10e7615dc6
mikeee <chocolatey@mike.ee>
2020-10-30 16:01:03 +0000

    AU: 1 updated - bootstrap-studio.install [skip ci] https://gist.github.com/17b5a042cba1bd9da2bf21615aab4f0f/60bdca3d4d68be3c0c35870f8a8b165678f9b9ee

--- automatic/bootstrap-studio.install/tools/chocolateyInstall.ps1
+++ automatic/bootstrap-studio.install/tools/chocolateyInstall.ps1
@@ -2,8 +2,8 @@
 
 $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
-    url            = 'https://bootstrapstudio.io/releases/desktop/5.4.1/Bootstrap%20Studio.exe'
-    checksum       = '247480a363358c3df8e401f1168412c4c6328b7ca577090e9afd9a7685d62ee8'
+    url            = 'https://bootstrapstudio.io/releases/desktop/5.4.2/Bootstrap%20Studio.exe'
+    checksum       = '36e338ed9454232c498babe19dd33f92c8ed3144ec2d2456e0df45b7f4675bf0'
     checksumType   = 'SHA256'
     fileType       = 'EXE'
     silentArgs     = '/S'
