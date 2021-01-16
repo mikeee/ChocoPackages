c8d63970b77092adf05eb9158ca66cc0c942dcac
mikeee <hey@mike.ee>
2021-01-16 19:07:11 +0000

    remove URL32 references

--- automatic/civo-cli/update.ps1
+++ automatic/civo-cli/update.ps1
@@ -7,9 +7,6 @@ $releases    = 'https://github.com/civo/cli/releases'
 function global:au_SearchReplace {
     @{
         ".\tools\chocolateyInstall.ps1" = @{
-            '(^\s*url\s*=\s*)(''.*'')'              = "`$1'$($Latest.URL32)'"
-            "(?i)(^\s*checksum\s*=\s*)('.*')"       = "`$1'$($Latest.Checksum32)'"
-            "(?i)(^\s*checksumType\s*=\s*)('.*')"   = "`$1'$($Latest.ChecksumType32)'"
             '(^\s*url64\s*=\s*)(''.*'')'            = "`$1'$($Latest.URL64)'"
             "(?i)(^\s*checksum64\s*=\s*)('.*')"       = "`$1'$($Latest.Checksum64)'"
             "(?i)(^\s*checksumType64\s*=\s*)('.*')"   = "`$1'$($Latest.ChecksumType64)'"
