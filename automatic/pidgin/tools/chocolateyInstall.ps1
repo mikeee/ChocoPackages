c10679528a592615ea9e2b160348b56b5007bf6b
mikeee <chocolatey@mike.ee>
2021-06-04 08:01:23 +0000

    AU: 1 updated - pidgin [skip ci] https://gist.github.com/17b5a042cba1bd9da2bf21615aab4f0f

--- automatic/pidgin/tools/chocolateyInstall.ps1
+++ automatic/pidgin/tools/chocolateyInstall.ps1
@@ -2,8 +2,8 @@
 
 $packageArgs = @{
     packageName   = $env:ChocolateyPackageName
-    url           = 'https://netcologne.dl.sourceforge.net/project/pidgin/Pidgin/2.14.4/pidgin-2.14.4-offline.exe'
-    checksum 		  = 'd10028cc572a40301265933583ae81f80afeba78c8e6726692c66185b9d7a85c'
+    url           = 'https://netcologne.dl.sourceforge.net/project/pidgin/Pidgin/2.14.5/pidgin-2.14.5-offline.exe'
+    checksum 		  = '797b12150d565d602a2cdea1626ba988094e8b15aedc03b3e7c45d941c23b850'
     checksumType 	= 'SHA256'
     fileType      = 'EXE'
     silentArgs    = '/S'
