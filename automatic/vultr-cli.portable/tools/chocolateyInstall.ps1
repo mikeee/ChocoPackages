1474b760b7e0c84437514a527b6f32f037ec8e19
mikeee <hey@mike.ee>
2021-07-02 15:19:28 +0100

    update vultr-cli package

--- automatic/vultr-cli.portable/tools/chocolateyInstall.ps1
+++ automatic/vultr-cli.portable/tools/chocolateyInstall.ps1
@@ -5,11 +5,8 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
     unzipLocation  = $toolsDir
-    url            = 'https://github.com//vultr/vultr-cli/releases/download/v0.4.0/vultr-cli_0.4.0_windows_32-bit.zip'
     url64          = 'https://github.com//vultr/vultr-cli/releases/download/v2.5.0/vultr-cli_2.5.0_windows_64-bit.zip'
 
-    checksum       = '7ba2676f8f1839fba21d7daf82b336c2df46e899153a61e6c310537fd9693690'
-    checksumType   = 'SHA256'
     checksum64     = '7c0359c7718ae4ba692e4e2dc6c1235a15b8804cbc20b4b9c84d8f724ee0648f'
     checksumType64 = 'SHA256'
 }
--- automatic/vultr-cli.portable/update.ps1
+++ automatic/vultr-cli.portable/update.ps1
@@ -7,9 +7,6 @@ $releases    = 'https://github.com/vultr/vultr-cli/releases'
 function global:au_SearchReplace {
     @{
         ".\tools\chocolateyInstall.ps1" = @{
-            '(^\s*url\s*=\s*)(''.*'')'              = "`$1'$($Latest.URL32)'"
-            "(?i)(^\s*checksum\s*=\s*)('.*')"       = "`$1'$($Latest.Checksum32)'"
-            "(?i)(^\s*checksumType\s*=\s*)('.*')"   = "`$1'$($Latest.ChecksumType32)'"
             '(^\s*url64\s*=\s*)(''.*'')'            = "`$1'$($Latest.URL64)'"
             "(?i)(^\s*checksum64\s*=\s*)('.*')"       = "`$1'$($Latest.Checksum64)'"
             "(?i)(^\s*checksumType64\s*=\s*)('.*')"   = "`$1'$($Latest.ChecksumType64)'"
@@ -18,8 +15,6 @@ function global:au_SearchReplace {
 }
 
 function global:au_BeforeUpdate {
-    $Latest.Checksum32 = Get-RemoteChecksum $Latest.Url32
-    $Latest.ChecksumType32 = 'SHA256'
     $Latest.Checksum64 = Get-RemoteChecksum $Latest.Url64
     $Latest.ChecksumType64 = 'SHA256'
 }
@@ -30,15 +25,11 @@ function global:au_AfterUpdate {
 
 function global:au_GetLatest {
     $page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-    $regexUrl32 = 'vultr-cli_(?<version>[\d\.]+)_windows_32-bit.zip'
     $regexUrl64 = 'vultr-cli_(?<version>[\d\.]+)_windows_64-bit.zip'
-
-    $url32 = $page.links | Where-Object href -match $regexUrl32 | Select-Object -First 1 -expand href
 
     $url64 = $page.links | Where-Object href -match $regexUrl64 | Select-Object -First 1 -expand href
 
     return @{
-        URL32        = "https://github.com/$url32"
         URL64        = "https://github.com/$url64"
         Version      = $matches.version
     }
