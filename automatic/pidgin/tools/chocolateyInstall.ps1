4555e755d11fd6871072b6ffe4a392a126ae418b
Brian Turek <brian.turek@gmail.com>
2021-10-21 07:18:54 -0400

    Update/fix Pidgin 2.14.8 checksum
    Pidgin v2.14.8 failed Chocolatey automated checks due to a bad checksum. Correct checksum can be found at https://sourceforge.net/projects/pidgin/files/Pidgin/2.14.8/pidgin-2.14.8-offline.exe.sha256sum

--- automatic/pidgin/tools/chocolateyInstall.ps1
+++ automatic/pidgin/tools/chocolateyInstall.ps1
@@ -3,7 +3,7 @@
 $packageArgs = @{
     packageName   = $env:ChocolateyPackageName
     url           = 'https://netcologne.dl.sourceforge.net/project/pidgin/Pidgin/2.14.8/pidgin-2.14.8-offline.exe'
-    checksum 		  = '8245f212a216ab49d52808079f8f882d787681a026d29b33f63f036d676bff67'
+    checksum 		  = 'b237de10d4fe5d44eabe97a7640d2a74b4d722a7cb2376cf5df2be632eda3f51'
     checksumType 	= 'SHA256'
     fileType      = 'EXE'
     silentArgs    = '/S'
