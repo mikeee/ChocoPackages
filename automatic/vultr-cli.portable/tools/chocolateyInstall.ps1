ee23059e0fe9c1a34d36154a88bfb12c97edeee7
mikeee <chocolatey@mike.ee>
2022-04-01 20:01:10 +0000

    AU: 1 updated - vultr-cli.portable [skip ci] https://gist.github.com/17b5a042cba1bd9da2bf21615aab4f0f/4a6b11ebb00e601202d9fa6bbe721641b52ad089

--- automatic/vultr-cli.portable/tools/chocolateyInstall.ps1
+++ automatic/vultr-cli.portable/tools/chocolateyInstall.ps1
@@ -5,9 +5,9 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
     unzipLocation  = $toolsDir
-    url64          = 'https://github.com//vultr/vultr-cli/releases/download/v2.8.2/vultr-cli_2.8.2_windows_64-bit.zip'
+    url64          = 'https://github.com//vultr/vultr-cli/releases/download/v2.12.2/vultr-cli_2.12.2_windows_64-bit.zip'
 
-    checksum64     = 'b08250228356e3603a782a2cf6f932a13bdf772ab5008d49160e7c3bfbe2fabc'
+    checksum64     = 'cb39954a9b5f4f91c565332d4d28125f60870b88be1cee2731fd5d6ca9aed79f'
     checksumType64 = 'SHA256'
 }
 
