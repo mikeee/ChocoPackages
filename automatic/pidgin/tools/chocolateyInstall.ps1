2a387be4de95d7c0e0c8716de04c951f2e72e584
Brian Turek <2017685+Caligatio@users.noreply.github.com>
2022-06-03 06:52:10 -0400

    Correct Pidgin installer checksum

--- automatic/pidgin/tools/chocolateyInstall.ps1
+++ automatic/pidgin/tools/chocolateyInstall.ps1
@@ -3,7 +3,7 @@
 $packageArgs = @{
     packageName   = $env:ChocolateyPackageName
     url           = 'https://netcologne.dl.sourceforge.net/project/pidgin/Pidgin/2.14.9/pidgin-2.14.9-offline.exe'
-    checksum 		  = 'a7b48fb28fa025b7526ecbc46d10be9a2390e089fb3f8cbc8bf4b821b180aeeb'
+    checksum 		  = '3bb3186222a491aae4da1c87544da30dab405b0f7f974bba6fd60963ec6c73c6'
     checksumType 	= 'SHA256'
     fileType      = 'EXE'
     silentArgs    = '/S'
