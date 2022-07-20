ca4d6e65df79a6894ca34e197642dd7384a1e4cc
mikeee <hey@mike.ee>
2022-07-20 17:43:09 +0100

    update op script

--- automatic/op/update.ps1
+++ automatic/op/update.ps1
@@ -17,13 +17,6 @@ function global:au_SearchReplace {
     }
 }
 
-function global:au_BeforeUpdate {
-    $Latest.Checksum32 = Get-RemoteChecksum $Latest.Url32
-    $Latest.ChecksumType32 = 'SHA256'
-    $Latest.Checksum64 = Get-RemoteChecksum $Latest.Url64
-    $Latest.ChecksumType64 = 'SHA256'
-}
-
 function global:au_AfterUpdate {
     Set-DescriptionFromReadme -SkipFirst 2
 }
@@ -34,12 +27,14 @@ function global:au_GetLatest {
     $regexUrl64 = '\/op_windows_amd64_v(?<version>[\d\.]+).zip'
 
     $url32 = $page.links | Where-Object href -match $regexUrl32 | Select-Object -First 1 -expand href
-
     $url64 = $page.links | Where-Object href -match $regexUrl64 | Select-Object -First 1 -expand href
+
+
 
     return @{
         URL32        = $url32
         URL64        = $url64
+
         Version      = $matches.version
     }
 }
