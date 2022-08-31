3971b1000e5f5f1015099fa80940a45aeb33dcff
mikeee <chocolatey@mike.ee>
2022-08-31 00:02:33 +0000

    AU: 1 updated - op [skip ci] https://gist.github.com/17b5a042cba1bd9da2bf21615aab4f0f/eda271268659e0bb48f592dda591bf2e88cd33af

--- automatic/op/tools/chocolateyInstall.ps1
+++ automatic/op/tools/chocolateyInstall.ps1
@@ -5,8 +5,8 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
     unzipLocation  = $toolsDir
-    url            = 'https://cache.agilebits.com/dist/1P/op2/pkg/v2.6.2/op_windows_386_v2.6.2.zip'
-    url64          = 'https://cache.agilebits.com/dist/1P/op2/pkg/v2.6.2/op_windows_amd64_v2.6.2.zip'
+    url            = 'https://cache.agilebits.com/dist/1P/op2/pkg/v2.7.0/op_windows_386_v2.7.0.zip'
+    url64          = 'https://cache.agilebits.com/dist/1P/op2/pkg/v2.7.0/op_windows_amd64_v2.7.0.zip'
 
     checksum       = ''
     checksumType   = ''
