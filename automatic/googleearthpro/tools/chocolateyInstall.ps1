2015de10dbe071ecfecd936a3ea05026c34e9f00
mikeee <chocolatey@mike.ee>
2022-09-25 18:00:38 +0000

    AU: 1 updated - googleearthpro [skip ci] https://gist.github.com/17b5a042cba1bd9da2bf21615aab4f0f/cc1082b2eff9734c5cfb8c07192a89367d1b9532

--- automatic/googleearthpro/tools/chocolateyInstall.ps1
+++ automatic/googleearthpro/tools/chocolateyInstall.ps1
@@ -3,11 +3,11 @@
 $packageArgs = @{
     packageName   = $env:ChocolateyPackageName
     url           = 'https://dl.google.com/dl/earth/client/advanced/current/googleearthprowin-7.3.4.exe'
-    checksum      = 'a8343a136b0ce4c130970e76d7e1ec562384ef0f0d7a6e9c2b61e93c6106d77c'
-    checksumType  = 'SHA256'
+    checksum      = 'fa46be3ca404015e56ff3219052afbe702ac2ce02130e49135753e8a0b25d5ae'
+    checksumType  = 'sha256'
     url64         = 'https://dl.google.com/dl/earth/client/advanced/current/googleearthprowin-7.3.4-x64.exe'
-    checksum64    = '25a53b44656f7d40a86a75dae64977c95ce99e59d523064a180e1fc8ec4b3439'
-    checksumType64= 'SHA256'
+    checksum64    = '2b1bdcb63b7239505e89ce2632555fa86851a47231a0e85b168245e56c02ba08'
+    checksumType64= 'sha256'
     fileType      = 'EXE'
     silentArgs    = 'OMAHA=1'
     validExitCodes = @(0)
