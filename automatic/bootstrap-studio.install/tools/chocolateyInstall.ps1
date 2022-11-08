1898fe1c481463489f2ceb12303a2148144dbad4
mikeee <chocolatey@mike.ee>
2022-11-08 16:01:31 +0000

    AU: 1 updated - bootstrap-studio.install [skip ci] https://gist.github.com/17b5a042cba1bd9da2bf21615aab4f0f/166719c9095bf4f198d8ef946f2aac89870012f0

--- automatic/bootstrap-studio.install/tools/chocolateyInstall.ps1
+++ automatic/bootstrap-studio.install/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 
 $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
-    url            = 'https://bootstrapstudio.io/releases/desktop/6.1.3/Bootstrap%20Studio.exe'
+    url            = 'https://bootstrapstudio.io/releases/desktop/6.2.0/Bootstrap%20Studio.exe'
     checksum       = ''
     checksumType   = ''
     fileType       = 'EXE'
