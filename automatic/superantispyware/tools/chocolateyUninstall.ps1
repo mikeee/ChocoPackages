7b6d4feab7b95e3785f294b03ea4ff0defafb1ea
mikeee <hey@mike.ee>
2022-11-15 00:49:37 +0000

    change tool dir

--- automatic/superantispyware/tools/chocolateyUninstall.ps1
+++ automatic/superantispyware/tools/chocolateyUninstall.ps1
@@ -5,15 +5,17 @@ $packageArgs = @{
     validExitCodes   = @(0)
     path             = "$env:ProgramFiles\SUPERAntiSpyware"
     path86           = "${env:ProgramFiles(x86)}\SUPERAntiSpyware"
-    toolsDir = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
+    toolsDir = "C:\tools"
 }
 
 if (Test-Path $path) {
     Invoke-WebRequest -Uri "https://www.superantispyware.com/downloads/SASUNINST.EXE" -OutFile $toolsDir
     Uninstall-ChocolateyPackage $packageName $installerType $silentArgs "$toolsDir\SASUNINST.exe"
+    Remove-Item -Path "$toolsDir\SASUNINST.exe"
 }
 
 if (Test-Path $path86) {
     Invoke-WebRequest -Uri "https://www.superantispyware.com/downloads/SASUNINST64.EXE" -OutFile $toolsDir
     Uninstall-ChocolateyPackage $packageName $installerType $silentArgs "$toolsDir\SASUNINST64.exe"
+    Remove-Item -Path "$toolsDir\SASUNINST64.exe"
 }
