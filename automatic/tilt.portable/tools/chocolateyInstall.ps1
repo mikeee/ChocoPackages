b80d7c39a5494b826e6119fa792b6d0963fb16d3
mikeee <hey@mike.ee>
2022-11-19 21:47:03 +0000

    create tilt.portable package

---
+++ automatic/tilt.portable/tools/chocolateyInstall.ps1
@@ -0,0 +1,14 @@
+﻿
+$ErrorActionPreference = 'Stop'
+
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+    packageName    = $env:ChocolateyPackageName
+    unzipLocation  = $toolsDir
+    url            = ''
+    checksum       = ''
+    checksumType   = 'SHA256'
+}
+
+Install-ChocolateyZipPackage @packageArgs
