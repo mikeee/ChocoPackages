c8eba17dacc86c7da732853dcf3d4fdd26bea9d4
mikeee <hey@mike.ee>
2022-11-20 18:13:37 +0000

    (meson) fix silent args syntax

--- automatic/meson/tools/chocolateyInstall.ps1
+++ automatic/meson/tools/chocolateyInstall.ps1
@@ -7,7 +7,7 @@ $packageArgs = @{
     checksum64       = ''
     checksumType64   = 'SHA256'
     fileType         = 'MSI'
-    silentArgs       = '/qn /norestart /l*v `"$(${ENV:TEMP})\$(${packageName}).$(${ENV:chocolateyPackageVersion}).MsiInstall.log`"'
+    silentArgs       = "/qn /norestart /l*v `"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
     validExitCodes   = @(0)
 }
 
