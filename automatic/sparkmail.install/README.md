b747ef2fbabc5c2b58bb0b67ae3c8a9275c4ad32
mikeee <hey@mike.ee>
2022-11-20 16:07:25 +0000

    (sparkmail.install) initial package

---
+++ automatic/sparkmail.install/README.md
@@ -0,0 +1,5 @@
+# ![Sparkmail Logo](https://cdn.jsdelivr.net/gh/mikeee/ChocoPackages/icons/sparkmail.png "sparkmail Logo") [sparkmail.install](https://chocolatey.org/packages/sparkmail.install)
+
+Spark is an email application by Readdle.
+
+**NOTE**: This package is automatically updated. If it is out of date and there is no newer package submitted, please use the `Contact Maintainers` functionality to alert them.
---
+++ automatic/sparkmail.install/tools/chocolateyInstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+    packageName   = $env:ChocolateyPackageName
+    url           = 'https://netcologne.dl.sourceforge.net/project/pidgin/Pidgin/2.14.9/pidgin-2.14.9-offline.exe'
+    checksum 		  = '3bb3186222a491aae4da1c87544da30dab405b0f7f974bba6fd60963ec6c73c6'
+    checksumType 	= 'sha256'
+    fileType      = 'EXE'
+    silentArgs    = '/S'
+    validExitCodes = @(0)
+}
+
+Stop-Process -ProcessName pidgin* -ErrorAction SilentlyContinue -Force
+Install-ChocolateyPackage @packageArgs
