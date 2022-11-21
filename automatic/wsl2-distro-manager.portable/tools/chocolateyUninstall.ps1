3d42e3d7decea8e465ab7156e8730fd3ac6d3045
mikeee <hey@mike.ee>
2022-11-21 01:47:25 +0000

    rename var

--- automatic/wsl2-distro-manager.portable/tools/chocolateyUninstall.ps1
+++ automatic/wsl2-distro-manager.portable/tools/chocolateyUninstall.ps1
@@ -1,13 +1,13 @@
 $ErrorActionPreference = 'Stop';
 
-$wsl2distromanagerExe = Get-ChildItem $(Split-Path -Parent $MyInvocation.MyCommand.Definition) | Where-Object -Property Name -Match "wsl2distromanager.exe"
+$pkgExe = Get-ChildItem $(Split-Path -Parent $MyInvocation.MyCommand.Definition) | Where-Object -Property Name -Match "wsl2distromanager.exe"
 
 if (-Not($tiltExe)) 
 {
     Write-Error -Message "wsl2distromanager exe is not found, please contact the maintainer of the package" -Category ResourceUnavailable
 }
 
-Write-Host "found wsl2distromanager exe in $($wsl2distromanagerExe.FullName)"
+Write-Host "found wsl2distromanager exe in $($pkgExe.FullName)"
 Write-Host "attempting to remove it" 
 
-Remove-Item $wsl2distromanagerExe.FullName
+Remove-Item $pkgExe.FullName
