310fa2b33a6c5cb69a981efe2356b1fc882911d7
mikeee <chocolatey@mike.ee>
2022-11-22 22:08:12 +0000

    AU: 3 updated - sparkmail tilt wsl2-distro-manager [skip ci] https://gist.github.com/17b5a042cba1bd9da2bf21615aab4f0f/b8dae9a2edf39b8260ffa2cef32ff7da5f0fc7d8

--- automatic/sparkmail/tools/chocolateyInstall.ps1
+++ automatic/sparkmail/tools/chocolateyInstall.ps1
@@ -2,9 +2,9 @@
 
 $packageArgs = @{
     packageName   = $env:ChocolateyPackageName
-    url           = ''
-    checksum 	  = ''
-    checksumType  = ''
+    url           = 'https://downloads.sparkmailapp.com/Spark3/win/dist/3.1.2.39231/Spark.exe'
+    checksum 	  = 'c9b865f16cc1df1f4d38a3e86d87d5bb954d33b6e7aed72f7abf51313a82810b'
+    checksumType  = 'SHA256'
     fileType      = 'EXE'
     silentArgs    = '/S'
     validExitCodes = @(0)
