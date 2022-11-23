857d16e2e73a405cbe7f2e08cbd41dbacb63fac3
mikeee <hey@mike.ee>
2022-11-23 05:22:48 +0000

    cruft to skip the dialogue box during uninstall

--- automatic/kakaotalk/tools/chocolateyUninstall.ps1
+++ automatic/kakaotalk/tools/chocolateyUninstall.ps1
@@ -1,12 +1,19 @@
 $ErrorActionPreference = 'Stop';
- 
-$packageArgs = @{
-  packageName     = $env:ChocolateyPackageName
-  fileType        = 'exe'
-  silentargs      = '/S'
-  validExitCodes  = @(0)
-  file            = 'C:\Program Files (x86)\Kakao\KakaoTalk\uninstall.exe'
 
+$silentargs          = '/S'
+$uninstaller32       = "${Env:ProgramFiles(x86)}\Kakao\KakaoTalk\uninstall.exe"
+$uninstaller64       = "${Env:ProgramFiles}\Kakao\KakaoTalk\uninstall.exe"
+
+if (Test-Path "$uninstaller64") {$file = "$uninstaller64"}
+if (Test-Path "$uninstaller32") {$file = "$uninstaller32"}
+
+if ((Test-Path "$uninstaller64") -or (Test-Path "$uninstaller32")) {
+  & $file $silentargs
+
+  $wshell = New-Object -ComObject WScript.Shell
+
+  while ($wshell.AppActivate("KakaoTalk Uninstall")) {
+    Start-Sleep -Seconds 1
+  }
+  $wshell.SendKeys("{ENTER}")
 }
-
-UnInstall-ChocolateyPackage @packageArgs
