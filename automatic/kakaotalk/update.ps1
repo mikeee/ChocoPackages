aa2795c6327bdcef877f6e410638d579edfcebc0
mikeee <hey@mike.ee>
2022-11-23 03:53:38 +0000

    initial kakaotalk package

---
+++ automatic/kakaotalk/update.ps1
@@ -0,0 +1,37 @@
+#import-module au
+
+. $PSScriptRoot\..\..\scripts\all.ps1
+
+$release    = 'https://app-pc.kakaocdn.net/talk/win32/KakaoTalk_Setup.exe'
+
+function global:au_SearchReplace {
+    @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            '(^\s*url\s*=\s*)(''.*'')'              = "`$1'$($Latest.URL)'"
+            "(?i)(^\s*checksum\s*=\s*)('.*')"       = "`$1'$($Latest.Checksum)'"
+            "(?i)(^\s*checksumType\s*=\s*)('.*')"   = "`$1'$($Latest.ChecksumType)'"
+        }
+    }
+}
+
+function global:au_BeforeUpdate {
+    $Latest.Checksum = Get-RemoteChecksum $Latest.URL
+    $Latest.ChecksumType = 'SHA256'
+}
+
+function global:au_AfterUpdate {
+    Set-DescriptionFromReadme -SkipFirst 2
+}
+
+function global:au_GetLatest {
+    $tmpFile = "$env:TEMP\KakaoTalk_Setup.exe"
+    Get-WebFile $release $tmpFile | Out-Null
+    $versionInfo = Get-Item $tmpFile | % versioninfo
+
+    return @{
+        URL        = $release
+        Version    = $versionInfo.ProductVersion
+    }
+}
+
+Update-Package
