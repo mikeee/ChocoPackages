5ac58c359c40bddebb9af9ebe9db0f6a985ceea0
mikeee <hey@mike.ee>
2022-11-23 06:36:52 +0000

    initial package

---
+++ automatic/endless-sky/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$toolsDir   = Join-Path -Path (Get-ToolsLocation) -ChildPath 'endless-sky'
+
+$packageArgs = @{
+    packageName    = $env:ChocolateyPackageName
+    url            = ''
+    url64          = ''
+    checksum       = ''
+    checksumType   = 'SHA256'
+    checksum64     = ''
+    checksumType64 = 'SHA256'
+
+    unzipLocation  = $toolsDir
+}
+
+Install-ChocolateyZipPackage @packageArgs
---
+++ automatic/endless-sky/update.ps1
@@ -0,0 +1,51 @@
+#import-module au
+
+. $PSScriptRoot\..\..\scripts\all.ps1
+
+$repoOwner = 'endless-sky'
+$repoName = 'endless-sky'
+
+function global:au_SearchReplace {
+    @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            '(^\s*url\s*=\s*)(''.*'')'              = "`$1'$($Latest.URL32)'"
+            "(?i)(^\s*checksum\s*=\s*)('.*')"       = "`$1'$($Latest.Checksum32)'"
+            "(?i)(^\s*checksumType\s*=\s*)('.*')"   = "`$1'$($Latest.ChecksumType32)'"
+            '(^\s*url64\s*=\s*)(''.*'')'            = "`$1'$($Latest.URL64)'"
+            "(?i)(^\s*checksum64\s*=\s*)('.*')"       = "`$1'$($Latest.Checksum64)'"
+            "(?i)(^\s*checksumType64\s*=\s*)('.*')"   = "`$1'$($Latest.ChecksumType64)'"
+        }
+    }
+}
+
+function global:au_BeforeUpdate {
+    $Latest.Checksum32 = Get-RemoteChecksum $Latest.Url32
+    $Latest.ChecksumType32 = 'SHA256'
+    $Latest.Checksum64 = Get-RemoteChecksum $Latest.Url64
+    $Latest.ChecksumType64 = 'SHA256'
+}
+
+function global:au_AfterUpdate {
+    Set-DescriptionFromReadme -SkipFirst 2
+}
+
+function global:au_GetLatest {
+    $release = Get-GitHubRelease -OwnerName $repoOwner -RepositoryName $repoName -Latest
+    
+    $version = $release.tag_name
+    # Remove the leading 'v' from the tag name if it exists
+    if ($version.StartsWith('v')) {
+        $version = $version.Substring(1) 
+    }
+
+    $asset64 = $release.assets | Where-Object name -Match 'endless-sky-win64-v([\d\.]+).zip'
+    $asset32 = $release.assets | Where-Object name -Match 'endless-sky-win32-v([\d\.]+).zip'
+
+    return @{
+        URL32        = $asset32.browser_download_url
+        URL64        = $asset64.browser_download_url
+        Version      = $version
+    }
+}
+
+update -ChecksumFor none
