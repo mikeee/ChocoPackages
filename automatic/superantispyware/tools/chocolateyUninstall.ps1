a3a8b54eac2614c70f5dfc4fc8b4d5ccdbc2b44f
mikeee <hey@mike.ee>
2022-11-26 23:24:50 +0000

    (sas) manually push checksums

--- automatic/superantispyware/tools/chocolateyUninstall.ps1
+++ automatic/superantispyware/tools/chocolateyUninstall.ps1
@@ -6,9 +6,9 @@ $path             = "$env:ProgramFiles\SUPERAntiSpyware"
 $path86           = "${env:ProgramFiles(x86)}\SUPERAntiSpyware"
 $toolsDir         = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url              = 'https://www.superantispyware.com/downloads/SASUNINST.EXE'
-$checksum         = ''
+$checksum         = '8456c509fc724251dcbe726ffcee0cb83d09311d6654d97d47eedfdbbe19e4f3'
 $url64            = 'https://www.superantispyware.com/downloads/SASUNINST64.EXE'
-$checksum64       = ''
+$checksum64       = 'bae07915c906c2d335e17f130cd388dc92a134279e6963bd73907734132640b3'
 
 Get-ChocolateyWebFile -PackageName $packageName -FileFullPath "$toolsDir\SASUNINST.exe" -Url $url -Url64bit $url64 -Checksum $checksum -Checksum64 - $checksum64
 
