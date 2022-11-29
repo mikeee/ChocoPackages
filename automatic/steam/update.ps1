74fa63c2db52c8f6fb38af7fd17deeacf0d05863
mikeee <hey@mike.ee>
2022-11-29 08:03:07 +0000

    change version to yyMMdd due to existing packages

--- automatic/steam/update.ps1
+++ automatic/steam/update.ps1
@@ -27,7 +27,7 @@ function global:au_GetLatest {
     $oldChecksum = (Select-String -Path '.\tools\chocolateyinstall.ps1' -Pattern "(checksum\s*=\s*)('.*')").Line.Split("'")[1]
 
     if ( $checksum -ne $oldChecksum ) {
-        $versionDate = Get-Date -Format "ddMMyy"
+        $versionDate = Get-Date -Format "yyMMdd"
         $finalVersion = (Get-Item $tempfile).VersionInfo.FileVersion + $versionDate
     } else {
         [xml]$xmlPackage = Get-Content -Path '.\steam.nuspec'
