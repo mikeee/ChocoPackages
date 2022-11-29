60febc206e60bfed643b43f822e4b50a51159207
mikeee <chocolatey@mike.ee>
2022-11-29 06:02:55 +0000

    AU: 1 updated - steam [skip ci] https://gist.github.com/17b5a042cba1bd9da2bf21615aab4f0f/73f49a18cdb188c6d61beb172af55596d18bbd9b

--- automatic/steam/tools/chocolateyInstall.ps1
+++ automatic/steam/tools/chocolateyInstall.ps1
@@ -1,9 +1,9 @@
-$ErrorActionPreference = 'Stop';
+﻿$ErrorActionPreference = 'Stop';
 
 $packageArgs = @{
     packageName   = $env:ChocolateyPackageName
-    url           = ''
-    checksum 	  = ''
+    url           = 'https://media.steampowered.com/client/installer/SteamSetup.exe'
+    checksum 	  = '3B616CB0BEAACFFB53884B5BA0453312D2577DB598D2A877A3B251125FB281A1'
     checksumType  = 'SHA256'
     fileType      = 'EXE'
     silentArgs    = '/S'
