66766929e86f08a18fee5a27785154499afa9b17
mikeee <chocolatey@mike.ee>
2022-11-29 10:08:27 +0000

    AU: 1 updated - asana [skip ci] https://gist.github.com/17b5a042cba1bd9da2bf21615aab4f0f/dee5321bf2f8d45f809d376fd175434608c28441

--- automatic/asana/tools/chocolateyInstall.ps1
+++ automatic/asana/tools/chocolateyInstall.ps1
@@ -1,12 +1,12 @@
-$ErrorActionPreference = 'Stop'
+﻿$ErrorActionPreference = 'Stop'
 
 $packageArgs = @{
     packageName     = $env:ChocolateyPackageName
-    url             = ''
-    checksum        = ''
+    url             = 'https://desktop-downloads.asana.com/win32_ia32/prod/latest/AsanaSetup.exe'
+    checksum        = 'cb346bc148a6b971e8684cc3576d33ab8eec7c941bdf67edebb256f61f32378b'
     checksumType    = 'SHA256'
-    url64           = ''
-    checksum64      = ''
+    url64           = 'https://desktop-downloads.asana.com/win32_x64/prod/latest/AsanaSetup.exe'
+    checksum64      = '08c8d072ff449b40b6af5cc83f889442a4547ae135a53cf54871c234e88f9b3c'
     checksumType64  = 'SHA256'
     fileType        = 'EXE'
     silentArgs      = '--silent'
