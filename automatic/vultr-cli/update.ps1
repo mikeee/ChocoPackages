30d5afcd51e1ed2e24b32bbacf045515bd79a414
mikeee <hey@mike.ee>
2022-12-03 01:15:27 +0000

    (vultr-cli) add package

---
+++ automatic/vultr-cli/update.ps1
@@ -0,0 +1,27 @@
+#import-module au
+
+function global:au_SearchReplace {
+    @{
+        "$($Latest.PackageName).nuspec" = @{
+            "(\<dependency .+?`"$($Latest.PackageName).portable`" version=)`"([^`"]+)`"" = "`$1`"$($Latest.Version)`""
+        }
+    }
+}
+
+function global:au_AfterUpdate {
+    Set-DescriptionFromReadme -SkipFirst 2
+}
+
+function global:au_GetLatest {
+    $packageName = $Latest.PackageName
+    $page = Invoke-WebRequest -Uri "https://chocolatey.org/packages/$packageName.portable/" -UseBasicParsing
+    $regexUrl = 'packages\/'+$packageName+'.portable\/(?<version>[\d.]+)\/ContactAdmins'
+
+    $page.links | Where-Object href -match $regexUrl | Select-Object -First 1 -expand href
+
+    return @{
+        Version      = $matches.version
+    }
+}
+
+update -ChecksumFor none
