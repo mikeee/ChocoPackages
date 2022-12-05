31d2d902d0f1b28120a081ec52a1d086197d1617
mikeee <chocolatey@mike.ee>
2022-12-05 16:09:49 +0000

    AU: 1 updated - adobeair [skip ci] https://gist.github.com/17b5a042cba1bd9da2bf21615aab4f0f/b57e63d110eb1f09fee943a6144ce93bc5f08a6d

--- automatic/adobeair/tools/chocolateyInstall.ps1
+++ automatic/adobeair/tools/chocolateyInstall.ps1
@@ -1,12 +1,12 @@
-$package = 'AdobeAIR'
+﻿$package = 'AdobeAIR'
 
 $params = @{
     packageName  = $package;
     fileType     = 'exe';
     silentArgs   = '-silent -eulaAccepted';
-    url          = ''
-    checksum     = ''
-    checksumType = 'sha256'
+    url          = 'https://airsdk.harman.com/assets/downloads/AdobeAIR.exe'
+    checksum     = 'd43321d814e744fd571f3f6360da070142c6a57afb4fe5eca0bd494c3384a38a'
+    checksumType = 'SHA256'
 }
 
 Install-ChocolateyPackage @params
