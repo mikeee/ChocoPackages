dda63d8e5215e4ce6616aae7417ff4894551048f
mikeee <chocolatey@mike.ee>
2022-12-07 17:14:31 +0000

    AU: 1 updated - clojure [skip ci] https://gist.github.com/17b5a042cba1bd9da2bf21615aab4f0f/3f00b2e298f210eeb1045e4f4ac84a5d0a1d8e02

--- automatic/clojure/tools/chocolateyInstall.ps1
+++ automatic/clojure/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
-$installDir = Split-Path $MyInvocation.MyCommand.Definition
+﻿$installDir = Split-Path $MyInvocation.MyCommand.Definition
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
-  url            = ''
-  checksum       = ''
-  checksumType   = 'sha256'
+  url            = 'https://download.clojure.org/install/clojure-tools-1.11.1.1200.zip'
+  checksum       = '6d121c6712d31c2fef2fbf477da22ff5b183a9687c698ffb79d094719d9c391e'
+  checksumType   = 'SHA256'
   unzipLocation  = $installDir
 }
 
