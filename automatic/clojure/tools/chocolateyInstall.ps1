931c2bf99e410c62bd166204bacc5943ae451d5c
mikeee <hey@mike.ee>
2022-12-07 17:05:52 +0000

    modify installer

--- automatic/clojure/tools/chocolateyInstall.ps1
+++ automatic/clojure/tools/chocolateyInstall.ps1
@@ -1,9 +1,15 @@
+$installDir = Split-Path $MyInvocation.MyCommand.Definition
+
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   url            = ''
   checksum       = ''
   checksumType   = 'sha256'
-  unzipLocation  = "$env:ProgramFiles\PowerShell\Modules"
+  unzipLocation  = $installDir
 }
 
 Install-ChocolateyZipPackage @packageArgs
+
+$psModulePath = [Environment]::GetEnvironmentVariable("PSModulePath")
+$psModulePath += $installDir
+[Environment]::SetEnvironmentVariable("PSModulePath",$psModulePath)
