8ed87fdf6f2e1d59d12d17b6d9ee7796bd30ea33
mikeee <hey@mike.ee>
2023-01-09 17:09:58 +0000

    (asana) update uninstall script

--- automatic/asana/tools/chocolateyUninstall.ps1
+++ automatic/asana/tools/chocolateyUninstall.ps1
@@ -1,10 +1,35 @@
-$packageArgs = @{
-  packageName     = $env:ChocolateyPackageName
-  fileType        = 'exe'
-  silentargs      = '--uninstall --silent'
-  validExitCodes  = @(0)
-  file            = "$env:LOCALAPPDATA\Asana\Update.exe"
+$ErrorActionPreference = 'Stop';
 
+$packageName = $env:ChocolateyPackageName
+$softwareName = 'Asana*'
+$installerType = 'exe'
+$silentArgs = '--uninstall --silent'
+$validExitCodes = @(0)
+
+$uninstalled = $false
+
+[array]$key = Get-UninstallRegistryKey -SoftwareName $softwareName
+
+if ($key.Count -eq 1) {
+  $key | ForEach-Object {
+    $file = "$($_.InstallLocation)\Update.exe"
+
+    if ($installerType -eq 'MSI') {
+      $silentArgs = "$($_.PSChildName) $silentArgs"
+      $file = ''
+    }
+
+    Uninstall-ChocolateyPackage -PackageName $packageName `
+                                -FileType $installerType `
+                                -SilentArgs "$silentArgs" `
+                                -ValidExitCodes $validExitCodes `
+                                -File "$file"
+  }
+} elseif ($key.Count -eq 0) {
+  Write-Warning "$packageName has already been uninstalled by other means."
+} elseif ($key.Count -gt 1) {
+  Write-Warning "$key.Count matches found!"
+  Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
+  Write-Warning "Please alert package maintainer(s) the following keys were matched:"
+  $key | ForEach-Object {Write-Warning "- $_.DisplayName"}
 }
-
-UnInstall-ChocolateyPackage @packageArgs
