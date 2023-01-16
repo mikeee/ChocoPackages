7c0c8fd8b26e678c9fba688a176ac7344db2330b
mikeee <chocolatey@mike.ee>
2023-01-16 15:51:25 +0000

    AU: 2 updated - civo-cli vscode-go-nightly [skip ci] https://gist.github.com/17b5a042cba1bd9da2bf21615aab4f0f/f779ebcfc15979cc2cf34dee8c0368f4108b8ad7

--- automatic/civo-cli/tools/chocolateyInstall.ps1
+++ automatic/civo-cli/tools/chocolateyInstall.ps1
@@ -6,8 +6,8 @@ $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
     unzipLocation  = $toolsDir
 
-    url64          = 'https://github.com//civo/cli/releases/download/v1.0.44/civo-1.0.44-windows-amd64.zip'
-    checksum64     = '81b7fccb6da900d0adb95f757e5c905a7a7dabee71fafcc151ed909a8efef2ad'
+    url64          = 'https://github.com//civo/cli/releases/download/v1.0.45/civo-1.0.45-windows-amd64.zip'
+    checksum64     = '2cceafb9dee8246091b77b944d93472b1c45fc3b6063667243c8f57318cb56c0'
     checksumType64 = 'SHA256'
 }
 
--- automatic/vscode-go-nightly/tools/chocolateyInstall.ps1
+++ automatic/vscode-go-nightly/tools/chocolateyInstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'golang.go-nightly@2023.1.404'
+Install-VsCodeExtension -extensionId 'golang.go-nightly@2023.1.1403'
