3ba3646c40a9c9ff89b8dcf4a09cdbfe457283a3
mikeee <chocolatey@mike.ee>
2023-01-20 00:04:40 +0000

    AU: 1 updated - sqlite-studio.portable [skip ci] https://gist.github.com/17b5a042cba1bd9da2bf21615aab4f0f/fec51e0c63eb18d89ddf9a7f47859d45b8f45a36

--- automatic/sqlite-studio.portable/tools/chocolateyInstall.ps1
+++ automatic/sqlite-studio.portable/tools/chocolateyInstall.ps1
@@ -5,11 +5,11 @@ $toolsDir   = Join-Path -Path (Get-ToolsLocation) -ChildPath 'sqlite-studio.port
 
 $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
-    url            = 'https://github.com/pawelsalawa/sqlitestudio/releases/download/3.4.2/sqlitestudio_i386-3.4.2.zip'
-    url64          = 'https://github.com/pawelsalawa/sqlitestudio/releases/download/3.4.2/sqlitestudio_x64-3.4.2.zip'
-    checksum       = '0a998a2c97275588348d920e907982bb52cca2065752203e9b5b14302feba985'
+    url            = 'https://github.com/pawelsalawa/sqlitestudio/releases/download/3.4.3/sqlitestudio_i386-3.4.3.zip'
+    url64          = 'https://github.com/pawelsalawa/sqlitestudio/releases/download/3.4.3/sqlitestudio_x64-3.4.3.zip'
+    checksum       = '711626cf946ec4a698d00d60910510bce5c6fa2f0a5bb186dd8ae8bc389bb27e'
     checksumType   = 'SHA256'
-    checksum64     = '647cd3d3095db72550c056158b26f9f501ce66b4fee4c4c95ade51fe8d33daf3'
+    checksum64     = '3e49bb744bef83cb50a01f88c6e739365507b11d0820cb9de9ec62a6413bc342'
     checksumType64 = 'SHA256'
 
     unzipLocation  = $toolsDir
