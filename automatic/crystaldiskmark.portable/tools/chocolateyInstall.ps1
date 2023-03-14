4c98d27d52a8ebed8b867aa6717fde14c5b14ac1
mikeee <hey@mike.ee>
2023-03-14 10:18:50 +0000

    (sparkmail) cleanup spec

---
+++ automatic/crystaldiskmark.portable/tools/chocolateyInstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+    packageName    = $env:ChocolateyPackageName
+    file           = Get-Item "$toolsDir\*.exe"
+    fileType       = 'EXE'
+    silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+    validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyInstallPackage @packageArgs
+
+Get-ChildItem $toolsDir\*.exe | ForEach-Object { Remove-Item $_ -ea 0; if (Test-Path $_) { Set-Content "$_.ignore" } }
---
+++ automatic/crystaldiskmark.portable/update.ps1
@@ -0,0 +1,50 @@
+#import-module au
+
+. $PSScriptRoot\..\..\scripts\all.ps1
+
+$releases    = 'https://osdn.net/projects/crystaldiskmark/releases/'
+
+function global:au_SearchReplace {
+    @{
+        ".\legal\VERIFICATION.txt" = @{
+            "(?i)(listed on\s*)\<.*\>" = "`${1}<$releases>"
+            "(?i)(Portable.+)\<.*\>"   = "`${1}<$($Latest.URL32)>"
+            "(?i)(checksum type:).*"   = "`${1} $($Latest.ChecksumType32)"
+            "(?i)(checksumZip:).*"      = "`${1} $($Latest.Checksum32)"
+        }
+    }
+}
+
+function global:au_BeforeUpdate {
+    Get-RemoteFiles -Purge
+}
+
+function global:au_AfterUpdate {
+    Set-DescriptionFromReadme -SkipFirst 2
+}
+
+function global:au_GetLatest {
+    $page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+    $regexUrl = '\/crystaldiskmark\/downloads\/(?<revision>[\d.]+)\/CrystalDiskMark(?<version>(?<major>\d_\d_\d)(?<minor>\S?)).zip\/'
+
+    $page.links | Where-Object href -match $regexUrl | Select-Object -First 1 -expand href
+
+    $versionDL = $matches.version
+    $versionMajor = $matches.major -replace '_', '.'
+    if ($matches.minor) {
+        $versionMinor = [int][char]$matches.minor
+        $versionChoco = $versionMajor + "." + $versionMinor
+    } else {
+        $versionChoco = $versionMajor
+    }
+    $revision   = $matches.revision
+
+    return @{
+        URL        = "https://osdn.net/frs/redir.php?m=dotsrc&f=crystaldiskmark/$revision/CrystalDiskMark$versionDL.exe"
+        Version    = $versionChoco
+        FileType   = 'exe'
+    }
+}
+
+update -ChecksumFor none
+
