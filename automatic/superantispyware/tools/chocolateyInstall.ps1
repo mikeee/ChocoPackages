15fc5f1bfab6e56aa2d1aeec2bef4095810ad1f0
mikeee <chocolatey@mike.ee>
2023-03-25 00:04:10 +0000

    AU: 1 updated - superantispyware [skip ci] https://gist.github.com/mikeee/17b5a042cba1bd9da2bf21615aab4f0f/bc2bc239113403e8e0ca4ceaa3ee490e0f79dd91

--- automatic/superantispyware/tools/chocolateyInstall.ps1
+++ automatic/superantispyware/tools/chocolateyInstall.ps1
@@ -3,7 +3,7 @@
 $packageArgs = @{
     packageName   = $env:ChocolateyPackageName
     url           = 'https://securedownloads.superantispyware.com/SUPERAntiSpyware.exe'
-    checksum      = 'c846095afc7a19fa7526e83b134698394db335a0d7e8c75f6cacd9a5fbd9cd5f'
+    checksum      = '1121369fc75267972cf1d04e7eb9a57391569d375010f90c096cbb88c08ca487'
     checksumType  = 'SHA256'
     fileType      = 'EXE'
     silentArgs    = '/SILENT /ALLUSERS'
