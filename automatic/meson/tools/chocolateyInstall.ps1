e0192fdd44491a80735d0c50a6fc5a570c62d534
mikeee <chocolatey@mike.ee>
2023-04-10 18:03:57 +0000

    AU: 1 updated - meson [skip ci] https://gist.github.com/mikeee/17b5a042cba1bd9da2bf21615aab4f0f/f4a01929b002e84c4a87a0b1bef36be35a48b1f4

--- automatic/meson/tools/chocolateyInstall.ps1
+++ automatic/meson/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@ $ErrorActionPreference = 'Stop'
 
 $packageArgs = @{
     packageName      = $env:ChocolateyPackageName
-    url64            = 'https://github.com/mesonbuild/meson/releases/download/1.0.1/meson-1.0.1-64.msi'
-    checksum64       = '9cd946dc4acbedb858bacab5cecacd8280f0c5c565efe59aa7d5a1f318bc2c0e'
+    url64            = 'https://github.com/mesonbuild/meson/releases/download/1.1.0/meson-1.1.0-64.msi'
+    checksum64       = '14c96ac1318979a877b3cc5d691ca3ba8f27046a33d203917c6edac7b2329c80'
     checksumType64   = 'SHA256'
     fileType         = 'MSI'
     silentArgs       = "/qn /norestart /l*v `"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
