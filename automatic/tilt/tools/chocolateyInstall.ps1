e8778cead803f7984608b524ba23204ed12cf559
mikeee <chocolatey@mike.ee>
2023-04-18 00:04:18 +0000

    AU: 2 updated - tilt wsl2-distro-manager [skip ci] https://gist.github.com/mikeee/17b5a042cba1bd9da2bf21615aab4f0f/60ddd018d2c44712a58d1a33ff5508e80c537668

--- automatic/tilt/tools/chocolateyInstall.ps1
+++ automatic/tilt/tools/chocolateyInstall.ps1
@@ -6,8 +6,8 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
     packageName      = $env:ChocolateyPackageName
     unzipLocation    = $toolsDir
-    url64            = 'https://github.com/tilt-dev/tilt/releases/download/v0.32.1/tilt.0.32.1.windows.x86_64.zip'
-    checksum64       = 'f9c1a82d227bea1cd5946d8a6cb41693aa5a829e01db8a5315a0c8132048112a'
+    url64            = 'https://github.com/tilt-dev/tilt/releases/download/v0.32.2/tilt.0.32.2.windows.x86_64.zip'
+    checksum64       = '4dbdb0ac283a63073fa2465d8c895222dbe99a8a6818eb2afb4bff368e34b525'
     checksumType64   = 'SHA256'
 }
 
