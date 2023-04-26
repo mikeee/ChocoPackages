7fc438be6624440446982db17efc68abbd05bad7
mikeee <hey@mike.ee>
2023-04-26 14:29:53 +0100

    initial zebradesigner package

---
+++ automatic/zebradesigner/README.md
@@ -0,0 +1,5 @@
+# ![ZebraDesigner Logo](https://cdn.jsdelivr.net/gh/mikeee/ChocoPackages/icons/zebradesigner.png "zebradesigner Logo") [sparkmail](https://chocolatey.org/packages/zebradesigner)
+
+ZebraDesigner is barcode label software
+
+**NOTE**: This package is automatically updated. If it is out of date and there is no newer package submitted, please use the `Contact Maintainers` functionality to alert them.
---
+++ automatic/zebradesigner/update.ps1
@@ -0,0 +1,45 @@
+#import-module au
+
+. $PSScriptRoot\..\..\scripts\all.ps1
+
+$releases    = 'https://www.zebra.com/us/en/support-downloads/printer-software/zebra-designer-3-downloads.html'
+
+function global:au_SearchReplace {
+    @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            '(^\s*url\s*=\s*)(''.*'')'              = "`$1'$($Latest.URL)'"
+            "(?i)(^\s*checksum\s*=\s*)('.*')"       = "`$1'$($Latest.Checksum)'"
+            "(?i)(^\s*checksumType\s*=\s*)('.*')"   = "`$1'$($Latest.ChecksumType)'"
+        }
+    }
+}
+
+function global:au_BeforeUpdate {
+    $Latest.Checksum = Get-RemoteChecksum $Latest.URL
+    $Latest.ChecksumType = 'SHA256'
+}
+
+function global:au_AfterUpdate {
+    Set-DescriptionFromReadme -SkipFirst 2
+}
+
+function global:au_GetLatest {
+    $page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+    $regexVersion = 'zebradesigner(?<versionmajor>[\d])-(?<versionminor>[\d]+).exe'
+
+    $matched = $page.Content -match $regexVersion
+
+    $versionMajor =
+
+    If ($False -ne $matchedurl) {
+        $url = -join("https://www.zebra.com/content/dam/zebra_new_ia/en-us/software-printer/productivity-apps/zebra-designer-3/zebradesigner", $matches["versionmajor"], "-", $matches["versionminor"], ".exe")
+        $version = $matches["versionmajor"], ".", $matches["versionminor"]
+    }
+
+    return @{
+        URL        = $url
+        Version    = $version
+    }
+}
+
+Update-Package
