bada01600ce976f2b512158ea2ca506d9e1dd0e6
mikeee <chocolatey@mike.ee>
2023-04-29 00:04:32 +0000

    AU: 2 updated - msiafterburner tilt [skip ci] https://gist.github.com/mikeee/17b5a042cba1bd9da2bf21615aab4f0f/764064b07131a19437fd3f3072d77e236f90e52a

--- automatic/tilt/tools/chocolateyInstall.ps1
+++ automatic/tilt/tools/chocolateyInstall.ps1
@@ -6,8 +6,8 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
     packageName      = $env:ChocolateyPackageName
     unzipLocation    = $toolsDir
-    url64            = 'https://github.com/tilt-dev/tilt/releases/download/v0.32.2/tilt.0.32.2.windows.x86_64.zip'
-    checksum64       = '4dbdb0ac283a63073fa2465d8c895222dbe99a8a6818eb2afb4bff368e34b525'
+    url64            = 'https://github.com/tilt-dev/tilt/releases/download/v0.32.3/tilt.0.32.3.windows.x86_64.zip'
+    checksum64       = '6dd00dc8b55b9fe914fbc217152ea3a0c0016ef15cbc5c9622fc77afb7b75caf'
     checksumType64   = 'SHA256'
 }
 
