f2271dfab3b0049cef02ea9e61dd802424d3511e
mikeee <chocolatey@mike.ee>
2023-05-05 09:06:02 +0000

    AU: 1 updated - bluestacks [skip ci] https://gist.github.com/mikeee/17b5a042cba1bd9da2bf21615aab4f0f/5052a362c32a838f1ddd481a00f6a9c1d21e3662

--- automatic/bluestacks/tools/chocolateyInstall.ps1
+++ automatic/bluestacks/tools/chocolateyInstall.ps1
@@ -2,11 +2,11 @@
 
 $packageArgs = @{
     packageName   = $env:ChocolateyPackageName
-    url           = ''
-    checksum      = ''
+    url           = 'https://cdn3.bluestacks.com/downloads/windows/nxt/5.11.1.1002/a7fc42682e92780335ddcabe6fd9b6e8/FullInstaller/x86/BlueStacksFullInstaller_5.11.1.1002_x86_native.exe'
+    checksum      = '7d8889d3312f3624e2ad40cb7b1c4f0144205927eeea28c65c2aba7211a2ce4f'
     checksumType  = 'sha256'
-    url64         = ''
-    checksum64    = ''
+    url64         = 'https://cdn3.bluestacks.com/downloads/windows/nxt/5.11.1.1002/a7fc42682e92780335ddcabe6fd9b6e8/FullInstaller/x64/BlueStacksFullInstaller_5.11.1.1002_amd64_native.exe'
+    checksum64    = 'e0a0d1f346d4c75def508bdb2cda1904f7e489c90cae88142284e9a4cc5e7d4f'
     checksumType64= 'sha256'
     fileType      = 'EXE'
     silentArgs    = '-s'
