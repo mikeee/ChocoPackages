a1acb16263174930ec7cbd7458c2258de50e8a20
mikeee <chocolatey@mike.ee>
2024-05-07 12:52:51 +0000

    AU: 1 updated - civo-cli [skip ci] https://gist.github.com/mikeee/17b5a042cba1bd9da2bf21615aab4f0f/ab66e9d35f323726c3ee15d3cf99a5213831c4ea

--- automatic/civo-cli/tools/chocolateyInstall.ps1
+++ automatic/civo-cli/tools/chocolateyInstall.ps1
@@ -6,8 +6,8 @@ $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
     unzipLocation  = $toolsDir
 
-    url64          = 'https://github.com//civo/cli/releases/download/v1.0.81/civo-1.0.81-windows-amd64.zip'
-    checksum64     = '9bb875de6740ebfee8b2655c6114ef185bc2c3d52d9b1c79ee5324fbd5ba0d8c'
+    url64          = 'https://github.com//civo/cli/releases/download/v1.0.82/civo-1.0.82-windows-amd64.zip'
+    checksum64     = '228ed85508d021730736fb3c1b53a8b35a70b29a06c8687962f7627c90fafffd'
     checksumType64 = 'SHA256'
 }
 
