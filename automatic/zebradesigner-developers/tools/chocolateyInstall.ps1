16d3afeb6d807283d31189fafbb7d360f774f009
mikeee <chocolatey@mike.ee>
2024-05-10 08:45:19 +0000

    AU: 1 updated - zebradesigner-developers [skip ci] https://gist.github.com/mikeee/17b5a042cba1bd9da2bf21615aab4f0f/c907051380531efd8113426e4a5f66eea9791a06

--- automatic/zebradesigner-developers/tools/chocolateyInstall.ps1
+++ automatic/zebradesigner-developers/tools/chocolateyInstall.ps1
@@ -2,8 +2,8 @@
 
 $packageArgs = @{
     packageName   = $env:ChocolateyPackageName
-    url           = 'https://www.zebra.com/content/dam/zebra_new_ia/en-us/software-printer/productivity-apps/zebradesigner-3-for-developers/zebradesigner3-developers-322629.exe'
-    checksum 	  = 'eca67ba41c0be0eb2023bf6da4155d95b1516070754cd3007f236e910e2bc783'
+    url           = 'https://www.zebra.com/content/dam/zebra_new_ia/en-us/software-printer/productivity-apps/zebradesigner-3-for-developers/zebradesigner3-developers-322649.exe'
+    checksum 	  = 'c0bfd7534fcfb283109fd78ce1a87ea06124baf28ce2819eb45d9abda107c285'
     checksumType  = 'SHA256'
     fileType      = 'EXE'
     silentArgs    = 'ICONDESKTOP=FALSE /s'
