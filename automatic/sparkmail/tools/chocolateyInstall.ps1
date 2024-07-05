4ad6cad813320b5926589b50f397996b40bafe92
mikeee <chocolatey@mike.ee>
2024-07-05 16:45:30 +0000

    AU: 2 updated - sparkmail vultr-cli.portable [skip ci] https://gist.github.com/mikeee/17b5a042cba1bd9da2bf21615aab4f0f/d14f11faa60bec471823969e3c4f0f4cb95d11cb

--- automatic/sparkmail/tools/chocolateyInstall.ps1
+++ automatic/sparkmail/tools/chocolateyInstall.ps1
@@ -2,8 +2,8 @@
 
 $packageArgs = @{
     packageName   = $env:ChocolateyPackageName
-    url           = 'https://downloads.sparkmailapp.com/Spark3/win/dist/3.16.6.78393/Spark.exe'
-    checksum 	  = '5a229c21b42e6208d0d38a6fdaad389dcd94ac99316c4eac5d4140dc183af915'
+    url           = 'https://downloads.sparkmailapp.com/Spark3/win/dist/3.16.7.78552/Spark.exe'
+    checksum 	  = 'bf46a9d12f63c228858f479f2d0b392a75ca4f1195a3c8b5014f593fd34e305e'
     checksumType  = 'SHA256'
     fileType      = 'EXE'
     silentArgs    = '/S'
--- automatic/vultr-cli.portable/tools/chocolateyInstall.ps1
+++ automatic/vultr-cli.portable/tools/chocolateyInstall.ps1
@@ -5,9 +5,9 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
     unzipLocation  = $toolsDir
-    url64          = 'https://github.com/vultr/vultr-cli/releases/download/v3.2.0/vultr-cli_v3.2.0_windows_amd64.zip'
+    url64          = 'https://github.com/vultr/vultr-cli/releases/download/v3.3.0/vultr-cli_v3.3.0_windows_amd64.zip'
 
-    checksum64     = 'ff3590b9e16fc7bf249ea60c3a3df48ccc68b59a0c58ff7cddbbe73914272749'
+    checksum64     = '843e4de42f2631fa42948e6367ad24857bb7f1d4f44e1bbf57b2b1c5c3dd7650'
     checksumType64 = 'SHA256'
 }
 
