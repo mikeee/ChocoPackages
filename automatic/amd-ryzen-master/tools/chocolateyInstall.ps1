d03ccffedda3fdef0f10f514352c3c2f6713f18a
mikeee <chocolatey@mike.ee>
2024-10-30 17:17:26 +0000

    AU: 1 updated - amd-ryzen-master [skip ci] https://gist.github.com/mikeee/17b5a042cba1bd9da2bf21615aab4f0f/b7d522c8acc263256daa9f3d03ea9a83aef87f29

--- automatic/amd-ryzen-master/tools/chocolateyInstall.ps1
+++ automatic/amd-ryzen-master/tools/chocolateyInstall.ps1
@@ -2,9 +2,9 @@
 
 $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
-    url            = 'https://download.amd.com/Desktop/AMD-Ryzen-Master.exe'
-    checksum       = '0006C60C686279217440234ACC5CA35E141C0756A13A70F734A563BFB02BB0AC'
-    checksumType   = 'SHA256'
+    url            = 'https://download.amd.com/Desktop/amd-ryzen-master-2-14-1-3286.exe'
+    checksum       = ''
+    checksumType   = ''
     fileType       = 'EXE'
     silentArgs     = '/S /v/qn'
     validExitCodes = @(0)
