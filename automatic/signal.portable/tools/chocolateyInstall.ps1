05246e0bd18127c2f47bba2e2769dadc077161ec
mikeee <chocolatey@mike.ee>
2024-11-12 13:00:53 +0000

    AU: 1 updated - signal.portable [skip ci] https://gist.github.com/mikeee/17b5a042cba1bd9da2bf21615aab4f0f/56ef4336bc524e486661709161656d4b6d2d134b

--- automatic/signal.portable/tools/chocolateyInstall.ps1
+++ automatic/signal.portable/tools/chocolateyInstall.ps1
@@ -1,11 +1,11 @@
-$ErrorActionPreference = 'Stop'
+﻿$ErrorActionPreference = 'Stop'
 
 $toolsDir   = Split-Path $MyInvocation.MyCommand.Definition
 
 $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
-    url64          = ''
-    checksum64     = ''
+    url64          = 'https://github.com/portapps/signal-portable/releases/download/7.32.0-8/signal-portable-win64-7.32.0-8.7z'
+    checksum64     = 'd2c0915882afbb42414fa6e82aeddeb109a7ebc205dd6959fb35558bc2d3bd1a'
     checksumType64 = 'SHA256'
     unzipLocation  = $toolsDir
 }
