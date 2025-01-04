cb2a78b4fc72d5d155ab95055fe8b08cbf1be094
mikeee <chocolatey@mike.ee>
2025-01-04 15:39:19 +0000

    AU: 1 updated - nvidia-broadcast [skip ci] https://gist.github.com/mikeee/17b5a042cba1bd9da2bf21615aab4f0f/de4f60568485c849d1699f902b09e0cea00b43b7

--- automatic/nvidia-broadcast/tools/chocolateyInstall.ps1
+++ automatic/nvidia-broadcast/tools/chocolateyInstall.ps1
@@ -2,8 +2,8 @@
 
 $packageArgs = @{
     packageName   = $env:ChocolateyPackageName
-    url64           = 'https://international.download.nvidia.com/Windows/broadcast/1.4.0.29/NVIDIA_Broadcast_v1.4.0.29.exe'
-    checksum64      = '46861c79f09ec26b800bd9989854377e0f95ce4eaed598742de036ab2e5f144f'
+    url64           = 'https://international.download.nvidia.com/Windows/broadcast/1.4.0.39/NVIDIA_Broadcast_v1.4.0.39.exe'
+    checksum64      = 'fc6bf49af9c079a2f865f17d8de767d0759e71827eb68663fe7e5936655a5af9'
     checksumType64  = 'SHA256'
     fileType      = 'EXE'
     silentArgs    = '-s'
