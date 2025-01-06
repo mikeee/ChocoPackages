b9c5cfc01cad12f8536a83910b9c553f5b79d486
mikeee <chocolatey@mike.ee>
2025-01-06 02:08:20 +0000

    AU: 1 updated - signal.portable [skip ci] https://gist.github.com/mikeee/17b5a042cba1bd9da2bf21615aab4f0f/9f0f5a4afda54268093edd2c4566355103378513

--- automatic/signal.portable/tools/chocolateyInstall.ps1
+++ automatic/signal.portable/tools/chocolateyInstall.ps1
@@ -4,8 +4,8 @@ $toolsDir   = Split-Path $MyInvocation.MyCommand.Definition
 
 $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
-    url64          = 'https://github.com/portapps/signal-portable/releases/download/7.32.0-8/signal-portable-win64-7.32.0-8.7z'
-    checksum64     = 'd2c0915882afbb42414fa6e82aeddeb109a7ebc205dd6959fb35558bc2d3bd1a'
+    url64          = 'https://github.com/portapps/signal-portable/releases/download/7.36.1-9/signal-portable-win64-7.36.1-9.7z'
+    checksum64     = 'e8bf81ec89fedcd3b75ed68034b7097b4e29aae77536d14c2d11bf098f85970d'
     checksumType64 = 'SHA256'
     unzipLocation  = $toolsDir
 }
