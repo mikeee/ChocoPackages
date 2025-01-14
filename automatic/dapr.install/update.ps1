2b5a74c58352d20dd33383bf9802f2772b2d341d
Mike Nguyen <hey@mike.ee>
2025-01-14 15:51:31 +0000

    feat: add dapr packages (#174)
    * feat: add dapr packages
    
    Signed-off-by: mikeee <hey@mike.ee>
    
    * fix: add missing request
    
    Signed-off-by: mikeee <hey@mike.ee>
    
    ---------
    
    Signed-off-by: mikeee <hey@mike.ee>

---
+++ automatic/dapr.install/update.ps1
@@ -0,0 +1,43 @@
+#import-module au
+
+. $PSScriptRoot\..\..\scripts\all.ps1
+
+$releases    = 'https://github.com/dapr/cli/releases'
+
+function global:au_SearchReplace {
+    @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            '(^\s*url64\s*=\s*)(''.*'')'            = "`$1'$($Latest.URL64)'"
+            "(?i)(^\s*checksum64\s*=\s*)('.*')"       = "`$1'$($Latest.Checksum64)'"
+            "(?i)(^\s*checksumType64\s*=\s*)('.*')"   = "`$1'$($Latest.ChecksumType64)'"
+        }
+    }
+}
+
+function global:au_BeforeUpdate {
+    $Latest.Checksum64 = Get-RemoteChecksum $Latest.URL64
+    $Latest.ChecksumType64 = 'SHA256'
+}
+
+function global:au_AfterUpdate {
+    Set-DescriptionFromReadme -SkipFirst 2
+}
+
+function global:au_GetLatest {
+    $page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+    $regexUrl64 = '\/dapr\/cli\/releases\/download\/v(?<version>[\d\.]+)\/dapr\.msi'
+
+    $matched = $page.links | Where-Object href -match $regexUrl64 | Select-Object -First 1 -expand href
+
+    If ($False -ne $matched) {
+        $version = $matches["version"]
+    }
+
+
+    return @{
+        URL64        = "https://github.com/dapr/cli/releases/download/v"+$matches.version+"/dapr.msi"
+        Version      = $version
+    }
+}
+
+update -ChecksumFor none
---
+++ automatic/dapr.portable/README.md
@@ -0,0 +1,9 @@
+# ![dapr Logo](https://cdn.jsdelivr.net/gh/mikeee/ChocoPackages/icons/dapr.png "dapr Logo") [dapr](https://chocolatey.org/packages/dapr)
+
+`dapr` is a command-line interface for running Dapr-related tasks.
+
+The Dapr CLI allows you to setup Dapr on your local dev machine or on a Kubernetes cluster, provides debugging support, and launches and manages Dapr instances.
+
+Dapr is a set of integrated APIs with built-in best practices and patterns to build distributed applications. Dapr increases your developer productivity by 20-40% with out-of-the-box features such as workflow, pub/sub, state management, secret stores, external configuration, bindings, actors, distributed lock, and cryptography. You benefit from the built-in security, reliability, and observability capabilities, so you don't need to write boilerplate code to achieve production-ready applications.
+
+**NOTE**: This package is automatically updated. If it is out of date and there is no newer package submitted, please use the `Contact Maintainers` functionality to alert me.
