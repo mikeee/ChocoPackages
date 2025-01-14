dc90f297e914f27783247547a67f212d01afffd5
mikeee <chocolatey@mike.ee>
2025-01-14 01:57:11 +0000

    AU: 1 updated - googleearthpro [skip ci] https://gist.github.com/mikeee/17b5a042cba1bd9da2bf21615aab4f0f/c83aa8e49a093b45bfeb03907f65e1b656f72637

--- automatic/googleearthpro/tools/chocolateyInstall.ps1
+++ automatic/googleearthpro/tools/chocolateyInstall.ps1
@@ -3,10 +3,10 @@
 $packageArgs = @{
     packageName   = $env:ChocolateyPackageName
     url           = 'https://dl.google.com/dl/earth/client/advanced/current/googleearthprowin-7.3.6.exe'
-    checksum      = '3f0ffcf6d8037655526c988eeaeef96852b75e5fd8c08785a044d12442ff96ce'
+    checksum      = 'c97c459d609a6114c696d703dac844ea0377cc64d04f88b08e5acc26fa91f779'
     checksumType  = 'sha256'
     url64         = 'https://dl.google.com/dl/earth/client/advanced/current/googleearthprowin-7.3.6-x64.exe'
-    checksum64    = '78bcfb435e018cf4e88167900e8cfd486208dc38b59663141f4195d4c352460c'
+    checksum64    = '663d2e45bdf7df41d3d9a355a99c121d10a3830a23fc66bc554f1c532f6e2238'
     checksumType64= 'sha256'
     fileType      = 'EXE'
     silentArgs    = 'OMAHA=1'
