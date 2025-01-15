3f2782ac880142e875f8beb8ef13e62b31862af6
mikeee <chocolatey@mike.ee>
2025-01-15 16:45:36 +0000

    AU: 1 updated - emsisoft-emergency-kit [skip ci] https://gist.github.com/mikeee/17b5a042cba1bd9da2bf21615aab4f0f/60dbf1b5c584ce419c624534f173820cc286e7da

--- automatic/emsisoft-emergency-kit/tools/chocolateyInstall.ps1
+++ automatic/emsisoft-emergency-kit/tools/chocolateyInstall.ps1
@@ -3,7 +3,7 @@
 $packageArgs = @{
     packageName   = $env:ChocolateyPackageName
     url           = 'https://dl.emsisoft.com/EmsisoftEmergencyKit.exe'
-    checksum      = '5f1717cd7549918b86aed8cce520daaa662ee640b0e8fd0fa73a793f54b8ee15'
+    checksum      = '3692b527b6082a6efffa3600056db4e08823c540c18c31cb80c8f8051abdb8f9'
     checksumType  = 'SHA256'
     fileType      = 'EXE'
     silentArgs    = '/s'
