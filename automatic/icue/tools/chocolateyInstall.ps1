82448e9a6934a6900c0c29a32a53f62292a2d31a
Mike Nguyen <hey@mike.ee>
2025-01-18 16:59:13 +0000

    fix: icue install script (#177)
    Signed-off-by: Mike Nguyen <hey@mike.ee>

--- automatic/icue/tools/chocolateyInstall.ps1
+++ automatic/icue/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  fileType      = 'MSI'
+  fileType      = 'EXE'
   url           = 'https://www3.corsair.com/software/CUE_V5/public/modules/windows/installer/Install%20iCUE.exe'
   checksum      = 'EC9D3ED6C05A94DD3E551F6A170136A3E931F659C64BAC82885CD88A05D1656C'
   checksumType  = 'SHA256'
