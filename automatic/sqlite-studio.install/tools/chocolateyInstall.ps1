c66ab2a9e63c1b71da101533ba0cdddb42bb729c
Mike Nguyen <hey@mike.ee>
2025-02-23 22:18:47 +0000

    feat: add sqlitestudio install package (#187)
    Closes: #180

---
+++ automatic/sqlite-studio.install/tools/chocolateyInstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+    packageName    = $env:ChocolateyPackageName
+    url            = ''
+    url64          = ''
+    checksum 	   = ''
+    checksumType   = 'SHA256'
+    checksum64 	   = ''
+    checksumType64 = 'SHA256'
+    fileType       = 'EXE'
+    silentArgs     = '--mode unattended --install_for all'
+    validExitCodes = @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
