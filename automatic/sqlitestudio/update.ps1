2538ee1e6774db8a0f8f5acde44e61f40ed83675
Mike Nguyen <hey@mike.ee>
2025-02-24 13:18:45 +0000

    fix(sqlitestudio): update portable ref

--- automatic/sqlitestudio/update.ps1
+++ automatic/sqlitestudio/update.ps1
@@ -3,14 +3,14 @@
 function global:au_SearchReplace {
     @{
         "$($Latest.PackageName).nuspec" = @{
-            "(\<dependency .+?`"sqlite-studio.portable`" version=)`"([^`"]+)`"" = "`$1`"$($Latest.Version)`""
+            "(\<dependency .+?`"sqlite-studio.install`" version=)`"([^`"]+)`"" = "`$1`"$($Latest.Version)`""
         }
     }
 }
 
 function global:au_GetLatest {
-    $page = Invoke-WebRequest -Uri "https://chocolatey.org/packages/sqlite-studio.portable/" -UseBasicParsing
-    $regexUrl = 'packages\/sqlite-studio.portable\/(?<version>[\d.]+)\/ContactAdmins'
+    $page = Invoke-WebRequest -Uri "https://chocolatey.org/packages/sqlite-studio.install/" -UseBasicParsing
+    $regexUrl = 'packages\/sqlite-studio.install\/(?<version>[\d.]+)\/ContactAdmins'
 
     $page.links | Where-Object href -match $regexUrl | Select-Object -First 1 -expand href
 
