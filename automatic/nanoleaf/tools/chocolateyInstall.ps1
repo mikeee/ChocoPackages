dbbefdbe879ae036166ed3d0760518f612908f6f
mikeee <chocolatey@mike.ee>
2025-05-12 13:49:17 +0000

    AU: 1 updated - nanoleaf [skip ci] https://gist.github.com/mikeee/17b5a042cba1bd9da2bf21615aab4f0f/697856f1ca7a55a0ce8f99d685e25fbbe4f5815b

--- automatic/nanoleaf/tools/chocolateyInstall.ps1
+++ automatic/nanoleaf/tools/chocolateyInstall.ps1
@@ -2,8 +2,8 @@
 
 $packageArgs = @{
     packageName   = $env:ChocolateyPackageName
-    url           = 'https://desktop-app-prod-1.s3.us-west-2.amazonaws.com/Nanoleaf+Desktop+Setup+2.2.1.exe'
-    checksum 	  = '03ACC156178F8831F57093B7C491337D31BFB5C666574700E517F92ABA042D70'
+    url           = 'https://desktop-app-prod-1.s3.us-west-2.amazonaws.com/Nanoleaf+Desktop+Setup+2.2.2.exe'
+    checksum 	  = 'DB36353918FC19B032FEE54DF879B91F5CFD3930EA51BF19FEE50586DB2E5B8E'
     checksumType  = 'SHA256'
     fileType      = 'EXE'
     silentArgs    = '/S'
