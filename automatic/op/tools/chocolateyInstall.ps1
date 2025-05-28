e527ac0a0b4f02367bf0242cb9b34553245cb31b
mikeee <chocolatey@mike.ee>
2025-05-28 10:44:29 +0000

    AU: 1 updated - op [skip ci] https://gist.github.com/mikeee/17b5a042cba1bd9da2bf21615aab4f0f/0235790b15e945466a46a3c243a051bcce6f96e3

--- automatic/op/tools/chocolateyInstall.ps1
+++ automatic/op/tools/chocolateyInstall.ps1
@@ -5,12 +5,12 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
     unzipLocation  = $toolsDir
-    url            = 'https://cache.agilebits.com/dist/1P/op2/pkg/v2.31.0/op_windows_386_v2.31.0.zip'
-    url64          = 'https://cache.agilebits.com/dist/1P/op2/pkg/v2.31.0/op_windows_amd64_v2.31.0.zip'
+    url            = 'https://cache.agilebits.com/dist/1P/op2/pkg/v2.31.1/op_windows_386_v2.31.1.zip'
+    url64          = 'https://cache.agilebits.com/dist/1P/op2/pkg/v2.31.1/op_windows_amd64_v2.31.1.zip'
 
-    checksum       = '302aa41ec467ede05d471614a512c40f1f73e4b8d2645e7eb022f90675436175'
+    checksum       = '33739da463138b52fd65992bdc8d6513f85e0b97c6c2bbeefb02fd18756ed3ff'
     checksumType   = 'SHA256'
-    checksum64     = '45a1f2180d272ca8d5182974fa618033691260b33fb1a53cb425d532b08374a3'
+    checksum64     = '7cf3a30bde910a8087052cd453186f80b72931b94fd4e90b4c6f5485c436b451'
     checksumType64 = 'SHA256'
 }
 
