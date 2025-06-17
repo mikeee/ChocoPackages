39eb2e542656a22ec2a235640c40486374d0b6d7
mikeee <chocolatey@mike.ee>
2025-06-17 17:06:16 +0000

    AU: 1 updated - local [skip ci] https://gist.github.com/mikeee/17b5a042cba1bd9da2bf21615aab4f0f/10a9bb9f50d931f8cd7b7af124b15a7085abfecb

--- automatic/local/tools/chocolateyInstall.ps1
+++ automatic/local/tools/chocolateyInstall.ps1
@@ -2,8 +2,8 @@
 
 $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
-    url            = 'https://cdn.localwp.com/releases-stable/9.2.4+6788/local-9.2.4-windows.exe'
-    checksum       = '1a8a74c7e572f53d9800abf6fbf804ed618d6df38dd1889db5a8a32c95db5ca8'
+    url            = 'https://cdn.localwp.com/releases-stable/9.2.5+6810/local-9.2.5-windows.exe'
+    checksum       = 'c06ddfd94299427bd76ab4d154724ca28d8755db75d7fb1c235f9bd1fe54fe1f'
     checksumType   = 'SHA256'
     fileType       = 'EXE'
     silentArgs     = '/S'
