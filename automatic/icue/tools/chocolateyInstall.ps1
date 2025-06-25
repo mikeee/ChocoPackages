e12e45a7be3da0d561334d199c60c04f0fb2a103
mikeee <chocolatey@mike.ee>
2025-06-25 18:57:31 +0000

    AU: 1 updated - icue [skip ci] https://gist.github.com/mikeee/17b5a042cba1bd9da2bf21615aab4f0f/66fa7b492a3d5f7a208b96ed98cfebdc19c486fc

--- automatic/icue/tools/chocolateyInstall.ps1
+++ automatic/icue/tools/chocolateyInstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'EXE'
   url           = 'https://www3.corsair.com/software/CUE_V5/public/modules/windows/installer/Install%20iCUE.exe'
-  checksum      = 'B9D8BF509AA04B0631392524B4F2692166DA0623C701D7A7055821A8A95E8BD9'
+  checksum      = '49C46E96404894B382E2856938E3A733CB23C71957AF6341334B1A0FB995C72F'
   checksumType  = 'SHA256'
 
   softwareName  = 'icue*'
