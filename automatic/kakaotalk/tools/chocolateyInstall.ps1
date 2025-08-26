e184ff39e1b50563af36e60c0f0fa6f55749a29d
mikeee <chocolatey@mike.ee>
2025-08-26 14:46:41 +0000

    AU: 1 updated - kakaotalk [skip ci] https://gist.github.com/mikeee/17b5a042cba1bd9da2bf21615aab4f0f/b4af4ca6609103e3437c22b1fbbdc85cb92534ff

--- automatic/kakaotalk/tools/chocolateyInstall.ps1
+++ automatic/kakaotalk/tools/chocolateyInstall.ps1
@@ -3,7 +3,7 @@
 $packageArgs = @{
     packageName   = $env:ChocolateyPackageName
     url           = 'https://app-pc.kakaocdn.net/talk/win32/KakaoTalk_Setup.exe'
-    checksum 	  = '33b6ca5d02753a262accba4b85f8e03ef393bc81ba2bcc80dd0df770693c31c6'
+    checksum 	  = '2eac75ddc86f2b099ec131e0813df4fe55affa993886e5551710fd1905d04ef5'
     checksumType  = 'SHA256'
     fileType      = 'EXE'
     silentArgs    = '/S'
