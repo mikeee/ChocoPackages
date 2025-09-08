3c70748548faa879aee49cdf3e1f2442d59683bd
mikeee <chocolatey@mike.ee>
2025-09-08 13:10:03 +0000

    AU: 1 updated - hcloud.portable [skip ci] https://gist.github.com/mikeee/17b5a042cba1bd9da2bf21615aab4f0f/768e310c73340a7c9f2dfaec0c02a42cdb45ad79

--- automatic/hcloud.portable/tools/chocolateyInstall.ps1
+++ automatic/hcloud.portable/tools/chocolateyInstall.ps1
@@ -5,12 +5,12 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
     unzipLocation  = $toolsDir
-    url            = 'https://github.com/hetznercloud/cli/releases/download/v1.51.0/hcloud-windows-386.zip'
-    url64          = 'https://github.com/hetznercloud/cli/releases/download/v1.51.0/hcloud-windows-amd64.zip'
+    url            = 'https://github.com/hetznercloud/cli/releases/download/v1.52.0/hcloud-windows-386.zip'
+    url64          = 'https://github.com/hetznercloud/cli/releases/download/v1.52.0/hcloud-windows-amd64.zip'
 
-    checksum       = '6f4e695d7e0326a4836d5791331858d7657f0ed5bb34e11815eda2b08de1bf42'
+    checksum       = 'aedceb5372bb32a280897ba9624e692baf370bebc42c361ade4210be68e23f7f'
     checksumType   = 'SHA256'
-    checksum64     = '41a700d1159764a5de54a19fab106e7514e1cb7389eea249431d92c5934099f5'
+    checksum64     = 'dcbae1503815f4755a67dcee4f932ece8c3469f8a3d27fbffc99c3dc286b1989'
     checksumType64 = 'SHA256'
 }
 
