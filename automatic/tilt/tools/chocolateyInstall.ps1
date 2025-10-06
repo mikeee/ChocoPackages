76ed57f33fe824fbddf7467b8493aee22c423587
mikeee <chocolatey@mike.ee>
2025-10-06 21:39:51 +0000

    AU: 1 updated - tilt [skip ci] https://gist.github.com/mikeee/17b5a042cba1bd9da2bf21615aab4f0f/0cc5071da119ca6d8f2d22fa4469c789f21f09a8

--- automatic/tilt/tools/chocolateyInstall.ps1
+++ automatic/tilt/tools/chocolateyInstall.ps1
@@ -6,8 +6,8 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
     packageName      = $env:ChocolateyPackageName
     unzipLocation    = $toolsDir
-    url64            = 'https://github.com/tilt-dev/tilt/releases/download/v0.35.1/tilt.0.35.1.windows.x86_64.zip'
-    checksum64       = 'd1cefb351b447f74310c78c92fa08da7461efb49b9a375a0214118e9ce7f2874'
+    url64            = 'https://github.com/tilt-dev/tilt/releases/download/v0.35.2/tilt.0.35.2.windows.x86_64.zip'
+    checksum64       = '70fbcbd91b0ebc27da246e5ff763d5e3b19b0de9642408b9e35f708fc40f75ed'
     checksumType64   = 'SHA256'
 }
 
