943b4c2a65387030a4b311d5272c0550f9691ba9
mikeee <chocolatey@mike.ee>
2026-02-10 21:17:18 +0000

    AU: 1 updated - golang [skip ci] https://gist.github.com/mikeee/17b5a042cba1bd9da2bf21615aab4f0f/11ffb792b8bbfae6f1d61514774bf70046f3933c

--- automatic/golang/tools/chocolateyInstall.ps1
+++ automatic/golang/tools/chocolateyInstall.ps1
@@ -3,11 +3,11 @@ $packageArgs = @{
 	packageName   = $env:ChocolateyPackageName
 	unzipLocation = $root
 	fileType      = 'msi'
-	url           = 'https://golang.org/dl/go1.25.7.windows-386.msi'
-	checksum      = '8c9cbfab99bdc2e4149c58c739ce03a50e2edcb18477dbc5b69c626c26a9a0ea'
+	url           = 'https://golang.org/dl/go1.26.0.windows-386.msi'
+	checksum      = '82c87c3562eb6e31548d04e06a48edc43c94c0306b0a25e314d292deb964a9e0'
 	checksumType  = 'SHA256'
-	url64         = 'https://golang.org/dl/go1.25.7.windows-amd64.msi'
-	checksum64    = 'c6055334f2a72650e405806234c26bcb4c57911a8f1960de0c045bb66a732a84'
+	url64         = 'https://golang.org/dl/go1.26.0.windows-amd64.msi'
+	checksum64    = 'cfb446aa6144566f3aac0e74f8a1b5d7936a2bfcd0ee47cc36afb0052db9c872'
 	checksumType64= 'SHA256'
 
 	silentArgs    = '/qn /norestart'
