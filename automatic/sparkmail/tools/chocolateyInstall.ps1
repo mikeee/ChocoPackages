ef9b7071b237e01356a8fad0f8859cda45f97da1
mikeee <chocolatey@mike.ee>
2026-02-18 11:22:19 +0000

    AU: 1 updated - sparkmail [skip ci] https://gist.github.com/mikeee/17b5a042cba1bd9da2bf21615aab4f0f/4afec5f5d80ffd443b8e5dbd7e362f0500bc1e47

--- automatic/sparkmail/tools/chocolateyInstall.ps1
+++ automatic/sparkmail/tools/chocolateyInstall.ps1
@@ -2,8 +2,8 @@
 
 $packageArgs = @{
     packageName   = $env:ChocolateyPackageName
-    url           = 'https://downloads.sparkmailapp.com/Spark3/win/dist/3.28.0.131340/Spark.exe'
-    checksum 	  = 'd417defb05656057a99d0fcd31d91dde8862850e76ce107e6d2de0ea771038cb'
+    url           = 'https://downloads.sparkmailapp.com/Spark3/win/dist/3.28.0.131408/Spark.exe'
+    checksum 	  = '8ff4e9e1b653670d2236192de367300015751168e35cb729099bbcc38cbf38b7'
     checksumType  = 'SHA256'
     fileType      = 'EXE'
     silentArgs    = '/S'
