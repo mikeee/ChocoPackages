55136e571dcaf44c2d69632b51eb863e06106488
mikeee <chocolatey@mike.ee>
2026-04-10 15:21:09 +0000

    AU: 2 updated - kakaotalk tilt [skip ci] https://gist.github.com/mikeee/17b5a042cba1bd9da2bf21615aab4f0f/74af75065aa85252c18efc989b589db521632623

--- automatic/tilt/tools/chocolateyInstall.ps1
+++ automatic/tilt/tools/chocolateyInstall.ps1
@@ -6,8 +6,8 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
     packageName      = $env:ChocolateyPackageName
     unzipLocation    = $toolsDir
-    url64            = 'https://github.com/tilt-dev/tilt/releases/download/v0.37.0/tilt.0.37.0.windows.x86_64.zip'
-    checksum64       = '4df1a3519e15dbe3514752da0bb7a1f4f0a7c0189347f8a5df9eb251222d179a'
+    url64            = 'https://github.com/tilt-dev/tilt/releases/download/v0.37.1/tilt.0.37.1.windows.x86_64.zip'
+    checksum64       = '29801d8b969a3797f8135e840c27a6e7351e3febcea00849ecbe303d4f2ac405'
     checksumType64   = 'SHA256'
 }
 
