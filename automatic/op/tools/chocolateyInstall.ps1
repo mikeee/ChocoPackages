2411450164884e00e2a5ee70ce8cfcc50867416e
mikeee <chocolatey@mike.ee>
2026-04-16 22:09:44 +0000

    AU: 2 updated - op vultr-cli.portable [skip ci] https://gist.github.com/mikeee/17b5a042cba1bd9da2bf21615aab4f0f/9a33df86556508f1f489a3e0a6e0331e3cffbf6a

--- automatic/op/tools/chocolateyInstall.ps1
+++ automatic/op/tools/chocolateyInstall.ps1
@@ -5,12 +5,12 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
     unzipLocation  = $toolsDir
-    url            = 'https://cache.agilebits.com/dist/1P/op2/pkg/v2.33.1/op_windows_386_v2.33.1.zip'
-    url64          = 'https://cache.agilebits.com/dist/1P/op2/pkg/v2.33.1/op_windows_amd64_v2.33.1.zip'
+    url            = 'https://cache.agilebits.com/dist/1P/op2/pkg/v2.34.0/op_windows_386_v2.34.0.zip'
+    url64          = 'https://cache.agilebits.com/dist/1P/op2/pkg/v2.34.0/op_windows_amd64_v2.34.0.zip'
 
-    checksum       = 'a9bdab71488a78170ef3a0c7cda1e436a073844a33660c4c27494210a227c977'
+    checksum       = 'db46e206c3316efcd0b26f03f78f57c20aa1c763cf99301802e84621b41a80cf'
     checksumType   = 'SHA256'
-    checksum64     = 'd013805ac1790ce34aeef564a5e3cb576aea09fa660c2f797b39d0e32f7800a2'
+    checksum64     = 'ea1fdea5bc52a8cff3f6b502c03ec5a567cd6e97d38fac35c511841e7dfd67fc'
     checksumType64 = 'SHA256'
 }
 
--- automatic/vultr-cli.portable/tools/chocolateyInstall.ps1
+++ automatic/vultr-cli.portable/tools/chocolateyInstall.ps1
@@ -5,9 +5,9 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
     unzipLocation  = $toolsDir
-    url64          = 'https://github.com/vultr/vultr-cli/releases/download/v3.9.2/vultr-cli_v3.9.2_windows_amd64.zip'
+    url64          = 'https://github.com/vultr/vultr-cli/releases/download/v3.10.0/vultr-cli_v3.10.0_windows_amd64.zip'
 
-    checksum64     = 'd2efe342268fe428ad8a24460cd886a3883759b6cd5d2ce76d7935f0d2b14a58'
+    checksum64     = '280fb041250e12bf603051cc6808d71202e58a9bcbc76e60a51d040812079078'
     checksumType64 = 'SHA256'
 }
 
