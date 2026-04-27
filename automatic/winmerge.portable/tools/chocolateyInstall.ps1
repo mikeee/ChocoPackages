1157fa0abfb4abf038089b269c33e9d4699ca7a2
mikeee <chocolatey@mike.ee>
2026-04-27 14:35:59 +0000

    AU: 1 updated - winmerge.portable [skip ci] https://gist.github.com/mikeee/17b5a042cba1bd9da2bf21615aab4f0f/3acf3bc01b3fdca5c2efddef1782e86fcde694c6

--- automatic/winmerge.portable/tools/chocolateyInstall.ps1
+++ automatic/winmerge.portable/tools/chocolateyInstall.ps1
@@ -5,12 +5,12 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
     unzipLocation  = $toolsDir
-    url            = 'https://github.com//WinMerge/winmerge/releases/download/v2.16.54.2/winmerge-2.16.54.2-exe.zip'
-    url64          = 'https://github.com//WinMerge/winmerge/releases/download/v2.16.54.2/winmerge-2.16.54.2-x64-exe.zip'
+    url            = 'https://github.com//WinMerge/winmerge/releases/download/v2.16.56/winmerge-2.16.56-exe.zip'
+    url64          = 'https://github.com//WinMerge/winmerge/releases/download/v2.16.56/winmerge-2.16.56-x64-exe.zip'
 
-    checksum       = '9463237ed5c8e4a4443f48c3a58e51ac09dc6b068f432478164f3beca9f3c847'
+    checksum       = '8a4b927f1a07ba59acd736c6f4d4d5a1ab36541d6d22db83c8c7a7c08db4adef'
     checksumType   = 'SHA256'
-    checksum64     = '3832f9db8d4d2210c3a049a8c30aed16ea152a329ae0c1b0dbf9308adbc9fc5e'
+    checksum64     = 'f32de18a5d50f78ff1fc884b1dc6488809a8fcc8f3dcf197fc265a142d5e35c5'
     checksumType64 = 'SHA256'
 }
 
