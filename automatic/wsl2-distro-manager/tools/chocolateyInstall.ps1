6842e6167638d8f2b0e4e956985c0586188e14fa
mikeee <chocolatey@mike.ee>
2026-09-10 19:51:28 +0000

    AU: 1 updated - wsl2-distro-manager [skip ci] https://gist.github.com/mikeee/17b5a042cba1bd9da2bf21615aab4f0f/14d745a1d4f1790f8bb9253a7796a2035a84026a

--- automatic/wsl2-distro-manager/tools/chocolateyInstall.ps1
+++ automatic/wsl2-distro-manager/tools/chocolateyInstall.ps1
@@ -6,8 +6,8 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
     packageName      = $env:ChocolateyPackageName
     unzipLocation    = $toolsDir
-    url64            = 'https://github.com/bostrot/wsl2-distro-manager/releases/download/v2.0.3/wsl2-distro-manager-v2.0.3.zip'
-    checksum64       = '119e05883bd2ef37f3ffaacc3544a872aa48918c95beaa6c44763f575e9346dc'
+    url64            = 'https://github.com/bostrot/wsl2-distro-manager/releases/download/v2.0.4/wsl2-distro-manager-v2.0.4.zip'
+    checksum64       = '32b712594a91717e1e0cb3da006e6ffd57158f8747721f0b153da504e6bea33c'
     checksumType64   = 'SHA256'
 }
 
